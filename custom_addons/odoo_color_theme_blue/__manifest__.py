b3afaf0a9089ec3aacd010ee597d6adb9d6ca57c
unknown <dante.h@hotmail.es>
2025-06-04 09:49:23 -0400

    cambios odoo color

---
+++ custom_addons/odoo_color_theme_blue/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Color Corporativo Azul",
+    "version": "17.0.1.0.0",
+    "summary": "Tema azul corporativo personalizado para Odoo 17",
+    "author": "ChatGPT",
+    "category": "Theme/Backend",
+    "depends": ["web"],
+    "data": [
+        "views/assets.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
