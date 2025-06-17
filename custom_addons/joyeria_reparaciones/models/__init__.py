9a7556d57b854a6b6b682c88f76fb453b63cdf73
unknown <dante.h@hotmail.es>
2025-06-17 17:04:39 -0400

    reporte nuevo5

--- custom_addons/joyeria_reparaciones/models/__init__.py
+++ custom_addons/joyeria_reparaciones/models/__init__.py
@@ -1,5 +1,6 @@
 from . import reparacion
 from . import joyeria_producto  # este es el nuevo archivo
 from . import wizard_reporte_responsables
+from . import reporte_salida_taller_wizard
 #from . import res_users
 
