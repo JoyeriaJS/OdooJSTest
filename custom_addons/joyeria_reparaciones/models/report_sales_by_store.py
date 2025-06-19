afc7987b930e0ab708401ded5431893cf82d0c85
unknown <dante.h@hotmail.es>
2025-06-19 14:52:03 -0400

    cambios campo responsable10

--- custom_addons/joyeria_reparaciones/models/report_sales_by_store.py
+++ custom_addons/joyeria_reparaciones/models/report_sales_by_store.py
@@ -1,25 +1,23 @@
+# report_sales_by_store.py
 # -*- coding: utf-8 -*-
 from odoo import api, models
 from collections import OrderedDict
-from odoo.exceptions import AccessError
 
 class ReportSalesByStore(models.AbstractModel):
     _name = 'report.joyeria_reparaciones.report_sales_by_store_template'
-    _description = 'Reporte de Ventas por Tienda y Mes'
+    _description = 'Ventas por Tienda y Mes'
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        if not self.env.user.has_group('base.group_system'):
-            raise AccessError("Sólo los administradores pueden generar este reporte.")
-        # recortes seleccionados
-        docs = self.env['joyeria.reparacion'].browse(docids)
+        docs = self.env['joyeria.reparacion'].browse(docids).filtered(lambda r: r.fecha_recepcion)
         groups = OrderedDict()
+        # Precios fijos
+        PRICE_ROSADO = 140000.0
+        PRICE_AMARILLO = 165000.0
 
         for rec in docs:
+            store = rec.local_tienda or 'Sin Tienda'
             dt = rec.fecha_recepcion
-            store = rec.local_tienda
-            if not dt or not store:
-                continue
             key = (store, dt.year, dt.month)
             if key not in groups:
                 groups[key] = {
@@ -28,33 +26,45 @@ class ReportSalesByStore(models.AbstractModel):
                     'month': dt.month,
                     'docs': [],
                     'sums': {
-                        'precio_unitario': 0.0,
-                        'extra': 0.0,
-                        'abono': 0.0,
-                        'saldo': 0.0,
+                        'peso_valor':     0.0,
+                        'precio_unitario':0.0,
+                        'extra':          0.0,
+                        'saldo':          0.0,
+                        'rosado':         0.0,
+                        'amarillo':       0.0,
+                        'total_metales':  0.0,
+                        'total_taller':   0.0,
                     }
                 }
             grp = groups[key]
-            grp['docs'].append(rec)
-            grp['sums']['precio_unitario'] += rec.precio_unitario or 0.0
-            grp['sums']['extra']             += rec.extra or 0.0
-            grp['sums']['abono']             += rec.abono or 0.0
-            # saldo = precio + extra – abono
-            grp['sums']['saldo']             += (rec.precio_unitario or 0.0) + (rec.extra or 0.0) - (rec.abono or 0.0)
+            weight = rec.peso_valor or 0.0
+            rosado    = weight * PRICE_ROSADO
+            amarillo  = weight * PRICE_AMARILLO
+            saldo     = (rec.precio_unitario or 0.0) + (rec.extra or 0.0) - (rec.abono or 0.0)
+            total_metales = rosado + amarillo
+            total_taller  = total_metales + (rec.cobro_interno or 0.0) + (rec.hechura or 0.0) + (rec.cobros_extras or 0.0)
 
-        # pasar a lista (para iteración ordenada en el template)
-        group_list = []
-        for (store, year, month), val in groups.items():
-            group_list.append({
-                'store': store,
-                'year': year,
-                'month': month,
-                'docs': val['docs'],
-                'sums': val['sums'],
+            grp['docs'].append({
+                'rec': rec,
+                'rosado': rosado,
+                'amarillo': amarillo,
+                'total_metales': total_metales,
+                'total_taller': total_taller,
+                'saldo': saldo,
+                'weight': weight,
             })
+            s = grp['sums']
+            s['peso_valor']      += weight
+            s['precio_unitario'] += rec.precio_unitario or 0.0
+            s['extra']           += rec.extra           or 0.0
+            s['saldo']           += saldo
+            s['rosado']          += rosado
+            s['amarillo']        += amarillo
+            s['total_metales']   += total_metales
+            s['total_taller']    += total_taller
 
         return {
-            'doc_ids':   docids,
-            'doc_model': 'joyeria.reparacion',
-            'groups':    group_list,
+            'doc_ids':  docids,
+            'doc_model':'joyeria.reparacion',
+            'groups':   list(groups.values()),
         }
