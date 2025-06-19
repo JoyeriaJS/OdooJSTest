dd0a4f68c80eca1c75b4de96061e206ca56ea5fb
unknown <dante.h@hotmail.es>
2025-06-19 14:24:35 -0400

    cambios campo responsable8

--- custom_addons/joyeria_reparaciones/models/reporte_salida_taller_wizard.py
+++ custom_addons/joyeria_reparaciones/models/reporte_salida_taller_wizard.py
@@ -1,3 +1,4 @@
+# reporte_salida_taller_wizard.py
 # -*- coding: utf-8 -*-
 from odoo import api, models
 from odoo.exceptions import AccessError
@@ -9,11 +10,14 @@ class ReportSalidaTaller(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # Sólo administradores
+        # --- Validación de permisos ---
         if not self.env.user.has_group('base.group_system'):
             raise AccessError("Sólo los administradores pueden generar este reporte.")
+        # --------------------------------
+
         docs = self.env['joyeria.reparacion'].browse(docids)
         groups = OrderedDict()
+
         for rec in docs:
             dt = rec.fecha_recepcion
             if not dt:
@@ -40,14 +44,13 @@ class ReportSalidaTaller(models.AbstractModel):
             grp['sums']['cobros_extras']        += rec.cobros_extras        or 0.0
             grp['sums']['total_salida_taller']  += rec.total_salida_taller  or 0.0
 
-        group_list = []
-        for (year, month), val in groups.items():
-            group_list.append({
-                'year':  year,
-                'month': month,
-                'docs':  val['docs'],
-                'sums':  val['sums'],
-            })
+        group_list = [{
+            'year':  year,
+            'month': month,
+            'docs':  val['docs'],
+            'sums':  val['sums'],
+        } for (year, month), val in groups.items()]
+
         return {
             'doc_ids':   docids,
             'doc_model': 'joyeria.reparacion',
