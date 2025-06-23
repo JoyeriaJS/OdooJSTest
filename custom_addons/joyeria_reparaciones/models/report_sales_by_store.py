9357f173659810af5e2248b03df08a5524927b7d
unknown <dante.h@hotmail.es>
2025-06-23 09:42:52 -0400

    cambios reporte pago_taller

--- custom_addons/joyeria_reparaciones/models/report_sales_by_store.py
+++ custom_addons/joyeria_reparaciones/models/report_sales_by_store.py
@@ -1,22 +1,24 @@
+# report_sales_by_store.py
 # -*- coding: utf-8 -*-
 from odoo import api, models
 from collections import OrderedDict
 
 class ReportSalesByStore(models.AbstractModel):
     _name = 'report.joyeria_reparaciones.report_sales_by_store_template'
-    _description = 'Ventas por Tienda y Mes'
+    _description = 'Ventas por Tienda y Mes (por Fecha de Firma)'
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        docs = self.env['joyeria.reparacion'].browse(docids).filtered(lambda r: r.fecha_recepcion)
+        # sólo registros con fecha de firma
+        docs = self.env['joyeria.reparacion'].browse(docids).filtered(lambda r: r.fecha_firma)
         groups = OrderedDict()
-        # Tarifas fijas por metal
+        # Tarifas fijas por metal (valor por gramo)
         PRICE_ROSADO   = 140000.0
         PRICE_AMARILLO = 165000.0
 
         for rec in docs:
             store = rec.local_tienda or 'Sin Tienda'
-            dt = rec.fecha_recepcion
+            dt = rec.fecha_firma
             key = (store, dt.year, dt.month)
             if key not in groups:
                 groups[key] = {
@@ -33,86 +35,59 @@ class ReportSalesByStore(models.AbstractModel):
                         'cobro_interno':     0.0,
                         'hechura':           0.0,
                         'cobros_extras':     0.0,
-                        'rosado_weight':     0.0,
-                        'amarillo_weight':   0.0,
-                        'blanco_weight':     0.0,
-                        'plata_weight':      0.0,
                         'rosado_value':      0.0,
                         'amarillo_value':    0.0,
-                        'blanco_value':      0.0,
-                        'plata_value':       0.0,
                         'total_metales':     0.0,
-                        'total_taller':      0.0,
+                        'pago_taller':       0.0,
                     }
                 }
             grp = groups[key]
 
-            # cálculo de pesos y valores
-            w_val   = rec.peso_valor or 0.0
-            w_ext   = rec.metales_extra or 0.0
-            weight_total = w_val + w_ext
+            w_val = rec.peso_valor or 0.0
+            w_ext = rec.metales_extra or 0.0
+            # valor de metales según metal_utilizado
+            val_rosado   = 0.0
+            val_amarillo = 0.0
+            total_metales = 0.0
 
-            rosado_w   = 0.0
-            amarillo_w = 0.0
-            blanco_w   = 0.0
-            plata_w    = 0.0
-            rosado_v   = 0.0
-            amarillo_v = 0.0
-
-            # según metal_utilizado
+            total_weight = w_val + w_ext
             if rec.metal_utilizado == 'oro 18k rosado':
-                rosado_w   = weight_total
-                rosado_v   = weight_total * PRICE_ROSADO
+                val_rosado = total_weight * PRICE_ROSADO
             elif rec.metal_utilizado == 'oro 18k amarillo':
-                amarillo_w = weight_total
-                amarillo_v = weight_total * PRICE_AMARILLO
-            elif rec.metal_utilizado == 'oro 18k blanco':
-                blanco_w   = weight_total
-                # sin valor definido -> 0
-            elif rec.metal_utilizado == 'plata':
-                plata_w    = weight_total
+                val_amarillo = total_weight * PRICE_AMARILLO
 
-            saldo           = (rec.precio_unitario or 0.0) + (rec.extra or 0.0) - (rec.abono or 0.0)
-            total_metales   = rosado_v + amarillo_v
-            total_taller    = total_metales + \
-                              (rec.cobro_interno or 0.0) + \
-                              (rec.hechura or 0.0) + \
-                              (rec.cobros_extras or 0.0)
+            total_metales = val_rosado + val_amarillo
 
-            # agregamos la fila
+            saldo = (rec.precio_unitario or 0.0) + (rec.extra or 0.0) - (rec.abono or 0.0)
+            pago_taller = total_metales + \
+                          (rec.cobro_interno or 0.0) + \
+                          (rec.hechura or 0.0) + \
+                          (rec.cobros_extras or 0.0)
+
             grp['docs'].append({
                 'rec': rec,
                 'peso_valor':       w_val,
                 'metales_extra':    w_ext,
                 'saldo':            saldo,
-                'rosado_weight':    rosado_w,
-                'amarillo_weight':  amarillo_w,
-                'blanco_weight':    blanco_w,
-                'plata_weight':     plata_w,
-                'rosado_value':     rosado_v,
-                'amarillo_value':   amarillo_v,
+                'rosado_value':     val_rosado,
+                'amarillo_value':   val_amarillo,
                 'total_metales':    total_metales,
-                'total_taller':     total_taller,
+                'pago_taller':      pago_taller,
             })
 
-            # acumuladores
             s = grp['sums']
-            s['peso_valor']      += w_val
-            s['metales_extra']   += w_ext
-            s['precio_unitario'] += rec.precio_unitario or 0.0
-            s['extra']           += rec.extra           or 0.0
-            s['saldo']           += saldo
-            s['cobro_interno']   += rec.cobro_interno   or 0.0
-            s['hechura']         += rec.hechura         or 0.0
-            s['cobros_extras']   += rec.cobros_extras   or 0.0
-            s['rosado_weight']   += rosado_w
-            s['amarillo_weight'] += amarillo_w
-            s['blanco_weight']   += blanco_w
-            s['plata_weight']    += plata_w
-            s['rosado_value']    += rosado_v
-            s['amarillo_value']  += amarillo_v
-            s['total_metales']   += total_metales
-            s['total_taller']    += total_taller
+            s['peso_valor']        += w_val
+            s['metales_extra']     += w_ext
+            s['precio_unitario']   += rec.precio_unitario or 0.0
+            s['extra']             += rec.extra or 0.0
+            s['saldo']             += saldo
+            s['cobro_interno']     += rec.cobro_interno or 0.0
+            s['hechura']           += rec.hechura or 0.0
+            s['cobros_extras']     += rec.cobros_extras or 0.0
+            s['rosado_value']      += val_rosado
+            s['amarillo_value']    += val_amarillo
+            s['total_metales']     += total_metales
+            s['pago_taller']       += pago_taller
 
         return {
             'doc_ids':   docids,
