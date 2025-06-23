0a5d55b6f98140f1c57899ba9777f02a0d5d2767
unknown <dante.h@hotmail.es>
2025-06-23 18:04:19 -0400

    integracion point of sale20

--- custom_addons/punto_de_venta/__manifest__.py
+++ custom_addons/punto_de_venta/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Point of Sale 2',
+    'name': 'Punto de venta',
     'version': '1.0.1',
     'category': 'Sales/Point of Sale',
     'sequence': 40,
