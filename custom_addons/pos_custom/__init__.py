6d64df17aaa7db84216a01f126ec94c8d5cf0b2f
unknown <dante.h@hotmail.es>
2025-06-23 16:21:26 -0400

    integracion point of sale17

--- custom_addons/pos_custom/__init__.py
+++ custom_addons/pos_custom/__init__.py
@@ -10,4 +10,3 @@ from . import wizard
 def uninstall_hook(env):
     #The search domain is based on how the sequence is defined in the _get_sequence_values method in /addons/point_of_sale/models/stock_warehouse.py
     env['ir.sequence'].search([('name', 'ilike', '%Picking POS%'), ('prefix', 'ilike', '%/POS/%')]).unlink()
-
