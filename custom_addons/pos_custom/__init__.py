9d6f41da7373a1471e23326d6d42633f605fe69c
unknown <dante.h@hotmail.es>
2025-06-23 17:08:36 -0400

    integracion point of sale18

--- custom_addons/pos_custom/__init__.py
+++ custom_addons/pos_custom/__init__.py
@@ -5,8 +5,17 @@ from . import models
 from . import controllers
 from . import report
 from . import wizard
+from odoo import api, SUPERUSER_ID
 
 
 def uninstall_hook(env):
     #The search domain is based on how the sequence is defined in the _get_sequence_values method in /addons/point_of_sale/models/stock_warehouse.py
     env['ir.sequence'].search([('name', 'ilike', '%Picking POS%'), ('prefix', 'ilike', '%/POS/%')]).unlink()
+
+# pos_custom/__init__.py
+def pre_init_hook(cr):
+    # eliminar el add-on instalado
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    mod = env['ir.module.module'].search([('name','=','point_of_sale')], limit=1)
+    if mod:
+        mod.button_uninstall()
