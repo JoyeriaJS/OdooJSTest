124affc2813ebb2142844124b56cfabdb374a675
unknown <dante.h@hotmail.es>
2025-06-25 16:59:02 -0400

    cambios punto de venta49

--- custom_addons/pos_monthly_rma_report/__manifest__.py
+++ custom_addons/pos_monthly_rma_report/__manifest__.py
@@ -2,14 +2,14 @@
 {
     'name': 'Reporte Mensual RMA + POS',
     'version': '1.0.0',
-    'author': 'Joyería Sebastián',
+    'author': 'Integración',
     'category': 'Point of Sale',
     'license': 'AGPL-3',
     'depends': ['point_of_sale', 'joyeria_reparaciones'],
     'data': [
-        'report/report_rma_monthly.xml',
-        'report/template_rma_monthly.xml',
-        'views/menu_rma_monthly.xml'
+        'report/rma_monthly_report_action.xml',
+        'report/rma_monthly_template.xml',
+        'views/menu_rma_monthly.xml',
     ],
     'installable': True,
     'application': False,
--- custom_addons/pos_monthly_rma_report/models/report_rma_monthly.py
+++ custom_addons/pos_monthly_rma_report/models/report_rma_monthly.py
@@ -3,31 +3,34 @@ from odoo import api, models, fields
 from collections import OrderedDict
 
 class ReportRmaMonthly(models.AbstractModel):
-    _name = 'report.pos_monthly_rma_report.template_rma_monthly'
+    _name = 'report.pos_monthly_rma_report.rma_monthly_template'
     _description = 'Reporte Mensual RMA + POS'
 
     @api.model
     def _get_report_values(self, docids, data=None):
         today = fields.Date.context_today(self)
         start = today.replace(day=1)
+        # RMA entries by reception date
         rmas = self.env['joyeria.reparacion'].search([
             ('fecha_recepcion', '>=', start),
             ('fecha_recepcion', '<=', today),
         ])
+        # POS orders by order date
         pos_orders = self.env['pos.order'].search([
             ('date_order', '>=', f"{start} 00:00:00"),
             ('date_order', '<=', f"{today} 23:59:59"),
         ])
         groups = OrderedDict()
         for r in rmas:
-            d = r.fecha_recepcion.strftime('%Y-%m-%d') if hasattr(r.fecha_recepcion, 'strftime') else str(r.fecha_recepcion)[:10]
+            d = r.fecha_recepcion[:10]
             groups.setdefault(d, {'date': d, 'rma_total': 0.0, 'pos_total': 0.0})
             groups[d]['rma_total'] += r.subtotal or 0.0
         for p in pos_orders:
-            d = p.date_order.strftime('%Y-%m-%d') if hasattr(p.date_order, 'strftime') else str(p.date_order)[:10]
+            d = p.date_order[:10]
             groups.setdefault(d, {'date': d, 'rma_total': 0.0, 'pos_total': 0.0})
             groups[d]['pos_total'] += p.amount_total or 0.0
-        sorted_groups = [groups[k] for k in sorted(groups)]
+        # Prepare sorted list
+        sorted_groups = [groups[d] for d in sorted(groups)]
         return {
             'doc_ids': docids,
             'doc_model': 'joyeria.reparacion',
