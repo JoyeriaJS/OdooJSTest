49d9d0d87942febb07a20c6403cdb75c45d413bc
unknown <dante.h@hotmail.es>
2025-06-25 12:33:01 -0400

    cambios punto de venta26

--- custom_addons/pos_daily_rma_report_fixed/__manifest__.py
+++ custom_addons/pos_daily_rma_report_fixed/__manifest__.py
@@ -8,9 +8,9 @@
     'depends': ['point_of_sale', 'joyeria_reparaciones'],
     'data': [
         'security/ir.model.access.csv',
-        'views/menu.xml',
         'views/pos_daily_rma_report_wizard_view.xml',
         'report/pos_daily_rma_report_template.xml',
+        'views/menu.xml',
     ],
     'installable': True,
     'application': False,
