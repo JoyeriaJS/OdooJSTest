c3819d5388776c709fbcecbd7a6da72c5941fc0c
unknown <dante.h@hotmail.es>
2025-06-25 15:32:23 -0400

    cambios punto de venta38

--- custom_addons/pos_monthly_rma_report/__manifest__.py
+++ custom_addons/pos_monthly_rma_report/__manifest__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Reporte Mensual RMA + POS',
-    'version': '1.0',
+    'version': '1.0.1',
     'author': 'Joyería Sebastián',
     'category': 'Point of Sale',
     'license': 'AGPL-3',
     'depends': ['point_of_sale', 'joyeria_reparaciones'],
     'data': [
-        'views/menu_and_report.xml',
+        'report/report_monthly_rma_pos.xml',
+        'report/template_monthly_rma_pos.xml',
+        'views/menu.xml',
     ],
     'installable': True,
     'application': False,
