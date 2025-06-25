8bd44cd0b066ffd02c9a492d289d5f25c792f2ca
unknown <dante.h@hotmail.es>
2025-06-25 16:49:46 -0400

    cambios punto de venta47

--- custom_addons/pos_monthly_rma_report/models/report_rma_monthly.py
+++ custom_addons/pos_monthly_rma_report/models/report_rma_monthly.py
@@ -3,7 +3,7 @@ from odoo import api, models, fields
 from collections import OrderedDict
 
 class ReportRmaMonthly(models.AbstractModel):
-    _name = 'report.pos_monthly_rma_report.rma_monthly_template'
+    _name = 'report.pos_monthly_rma_report_fix.rma_monthly_template'
     _description = 'Reporte Mensual RMA + POS'
 
     @api.model
@@ -22,14 +22,21 @@ class ReportRmaMonthly(models.AbstractModel):
         ])
         groups = OrderedDict()
         for r in rmas:
-            d = r.fecha_recepcion[:10]
+            date_obj = r.fecha_recepcion
+            if hasattr(date_obj, 'strftime'):
+                d = date_obj.strftime('%Y-%m-%d')
+            else:
+                d = str(date_obj)[:10]
             groups.setdefault(d, {'date': d, 'rma_total': 0.0, 'pos_total': 0.0})
             groups[d]['rma_total'] += r.subtotal or 0.0
         for p in pos_orders:
-            d = p.date_order[:10]
+            date_obj = p.date_order
+            if hasattr(date_obj, 'strftime'):
+                d = date_obj.strftime('%Y-%m-%d')
+            else:
+                d = str(date_obj)[:10]
             groups.setdefault(d, {'date': d, 'rma_total': 0.0, 'pos_total': 0.0})
             groups[d]['pos_total'] += p.amount_total or 0.0
-        # Prepare sorted list
         sorted_groups = [groups[d] for d in sorted(groups)]
         return {
             'doc_ids': docids,
