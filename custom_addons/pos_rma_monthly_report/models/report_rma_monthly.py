d05d9efd90d5958f1e3dccced2d7a2153f4bd58b
unknown <dante.h@hotmail.es>
2025-06-25 16:07:55 -0400

    cambios punto de venta44

--- custom_addons/pos_rma_monthly_report/models/report_rma_monthly.py
+++ custom_addons/pos_rma_monthly_report/models/report_rma_monthly.py
@@ -10,30 +10,25 @@ class ReportRmaMonthly(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         today = fields.Date.context_today(self)
         start = today.replace(day=1)
-        # Reparaciones in period
         rmas = self.env['joyeria.reparacion'].search([
             ('fecha_recepcion', '>=', start),
             ('fecha_recepcion', '<=', today),
         ])
-        # POS orders in period
         poses = self.env['pos.order'].search([
             ('date_order', '>=', f"{start} 00:00:00"),
             ('date_order', '<=', f"{today} 23:59:59"),
         ])
         groups = OrderedDict()
-        # accumulate RMA totals by fecha_recepcion date
         for r in rmas:
             d = r.fecha_recepcion[:10]
             if d not in groups:
                 groups[d] = {'date': d, 'rma': 0.0, 'pos': 0.0}
             groups[d]['rma'] += r.subtotal or 0.0
-        # accumulate POS totals by order date
         for p in poses:
             d = p.date_order[:10]
             if d not in groups:
                 groups[d] = {'date': d, 'rma': 0.0, 'pos': 0.0}
             groups[d]['pos'] += p.amount_total or 0.0
-        # sorted list
         sorted_data = [groups[d] for d in sorted(groups)]
         return {
             'doc_ids': docids,
