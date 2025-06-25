26f9032b7f4096e7c7c8eebe5e1fe4f26bb6d85e
unknown <dante.h@hotmail.es>
2025-06-25 10:39:58 -0400

    cambios punto de venta10

---
+++ custom_addons/pos_qr_auth/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'POS QR Auth',
+    'version': '1.0',
+    'author': 'Joyería Sebastián',
+    'category': 'Point of Sale',
+    'depends': ['point_of_sale', 'joyeria_reparaciones'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/pos_qr_auth_views.xml',
+        'views/rma_pos_report_wizard_view.xml',
+        'views/report_rma_pos.xml',
+    ],
+    'assets': {
+        'point_of_sale.assets_backend': [
+            'pos_qr_auth/static/src/js/qr_auth.js',
+        ],
+    },
+    'installable': True,
+    'application': False,
+}
