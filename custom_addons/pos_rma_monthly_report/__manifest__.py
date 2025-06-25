6aef3b86e4e23e6ee81f7d89bdf8ecdde85f2064
unknown <dante.h@hotmail.es>
2025-06-25 16:00:38 -0400

    cambios punto de venta45

--- custom_addons/pos_rma_monthly_report/__manifest__.py
+++ custom_addons/pos_rma_monthly_report/__manifest__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Reporte Mensual RMA + POS',
-    'version': '1.0.1',
+    'version': '1.0.2',
     'author': 'Joyería Sebastián',
     'category': 'Point of Sale',
     'license': 'AGPL-3',
     'depends': ['point_of_sale', 'joyeria_reparaciones'],
     'data': [
-        'report/report_monthly_rma_pos.xml',
-        'report/template_monthly_rma_pos.xml',
-        'views/menu.xml',
+        'views/menu_rma_monthly.xml',
+        'report/report_rma_monthly.xml',
+        'report/template_rma_monthly.xml',
     ],
     'installable': True,
     'application': False,
--- custom_addons/pos_rma_monthly_report/models/report_rma_monthly.py
+++ custom_addons/pos_rma_monthly_report/models/report_rma_monthly.py
@@ -2,38 +2,43 @@
 from odoo import api, models, fields
 from collections import OrderedDict
 
-class ReportMonthlyRmaPos(models.AbstractModel):
-    _name = 'report.pos_monthly_rma_report.template_monthly_rma_pos'
+class ReportRmaMonthly(models.AbstractModel):
+    _name = 'report.pos_rma_monthly_report.template_rma_monthly'
     _description = 'Reporte Mensual RMA + POS'
 
     @api.model
     def _get_report_values(self, docids, data=None):
         today = fields.Date.context_today(self)
         start = today.replace(day=1)
+        # Reparaciones in period
         rmas = self.env['joyeria.reparacion'].search([
             ('fecha_recepcion', '>=', start),
             ('fecha_recepcion', '<=', today),
         ])
+        # POS orders in period
         poses = self.env['pos.order'].search([
             ('date_order', '>=', f"{start} 00:00:00"),
             ('date_order', '<=', f"{today} 23:59:59"),
         ])
         groups = OrderedDict()
+        # accumulate RMA totals by fecha_recepcion date
         for r in rmas:
             d = r.fecha_recepcion[:10]
             if d not in groups:
                 groups[d] = {'date': d, 'rma': 0.0, 'pos': 0.0}
             groups[d]['rma'] += r.subtotal or 0.0
+        # accumulate POS totals by order date
         for p in poses:
             d = p.date_order[:10]
             if d not in groups:
                 groups[d] = {'date': d, 'rma': 0.0, 'pos': 0.0}
             groups[d]['pos'] += p.amount_total or 0.0
-        sorted_groups = [groups[d] for d in sorted(groups)]
+        # sorted list
+        sorted_data = [groups[d] for d in sorted(groups)]
         return {
             'doc_ids': docids,
             'doc_model': 'joyeria.reparacion',
-            'groups': sorted_groups,
             'date_start': start,
             'date_stop': today,
+            'groups': sorted_data,
         }
