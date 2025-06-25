16f9b7440a246074a59e6cfbb7381bcec8e86ddc
unknown <dante.h@hotmail.es>
2025-06-25 09:58:35 -0400

    cambios punto de venta6

--- custom_addons/pos_qr_auth_no_wizard_v3/__manifest__.py
+++ custom_addons/pos_qr_auth_no_wizard_v3/__manifest__.py
@@ -8,8 +8,7 @@
         'security/ir.model.access.csv',
         'views/pos_qr_auth_views.xml',
         'views/report_rma_pos.xml',
-        'views/pos_order_extend_views.xml',
-    ],
+            ],
     'assets': {
         'point_of_sale.assets_backend': [
             'pos_qr_auth/static/src/js/qr_auth.js',
