32c2e57984f92af2bf332c82be35ca95567672d3
unknown <dante.h@hotmail.es>
2025-06-26 09:54:20 -0400

    cambios rma_monthly6

---
+++ custom_addons/pos_monthly_rma_report/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Monthly RMA + POS Report',
+    'summary': 'Reporte mensual combinando RMA y POS',
+    'version': '1.0',
+    'category': 'Point of Sale',
+    'author': 'ChatGPT',
+    'depends': ['base', 'point_of_sale', 'joyeria_reparaciones', 'report'],
+    'data': [
+        'views/menu_rma_monthly.xml',
+        'views/report_action.xml',
+        'views/report_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
