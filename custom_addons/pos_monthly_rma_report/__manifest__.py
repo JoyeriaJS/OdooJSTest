d5761114485bafd8819c090fe9ef0842ec2c25b1
unknown <dante.h@hotmail.es>
2025-06-26 10:00:57 -0400

    cambios rma_monthly6

--- custom_addons/pos_monthly_rma_report/__manifest__.py
+++ custom_addons/pos_monthly_rma_report/__manifest__.py
@@ -4,7 +4,7 @@
     'version': '1.0',
     'category': 'Point of Sale',
     'author': 'ChatGPT',
-    'depends': ['base', 'point_of_sale', 'joyeria_reparaciones', 'report'],
+    'depends': ['base', 'point_of_sale', 'joyeria_reparaciones'],
     'data': [
         'views/menu_rma_monthly.xml',
         'views/report_action.xml',
