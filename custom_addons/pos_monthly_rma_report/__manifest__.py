894621c62819ad2a0a32a5ecaeb45b50f7abdc66
unknown <dante.h@hotmail.es>
2025-06-26 09:41:44 -0400

    cambios rma_monthly4

--- custom_addons/pos_monthly_rma_report/__manifest__.py
+++ custom_addons/pos_monthly_rma_report/__manifest__.py
@@ -1,14 +1,14 @@
 {
-    'name': 'Reporte Mensual RMA + POS',
-    'version': '1.0',
-    'category': 'Point of Sale',
-    'summary': 'Reporte mensual de ventas POS y RMA',
-    'depends': ['point_of_sale', 'joyeria_reparaciones'],
-    'data': [
-        'report/report.xml',
-        'report/rma_monthly_template.xml',
-        'views/menu_rma_monthly.xml',
+    "name": "Reporte Mensual RMA + POS",
+    "version": "1.0",
+    "category": "Point of Sale",
+    "summary": "Reporte mensual que suma reparaciones (RMA) y ventas POS por mes",
+    "depends": ["point_of_sale", "joyeria_reparaciones"],
+    "data": [
+        "report/report_rma_monthly_action.xml",
+        "report/rma_monthly_template.xml",
+        "views/menu_rma_monthly.xml"
     ],
-    'installable': True,
-    'application': False,
+    "installable": true,
+    "application": false
 }
