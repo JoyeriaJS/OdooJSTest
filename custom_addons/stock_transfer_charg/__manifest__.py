7e701b99bac1c3744d69388ab79980a9e49a5e46
unknown <dante.h@hotmail.es>
2025-06-27 14:58:32 -0400

    cambios agrego modulo de reporte de cobro entre locales13

--- custom_addons/stock_transfer_charg/__manifest__.py
+++ custom_addons/stock_transfer_charg/__manifest__.py
@@ -5,8 +5,8 @@
     'summary': 'Reporte de cobros entre locales basado en traspasos de inventario',
     'depends': ['stock', 'joyeria_reparaciones'],
     'data': [
-        'report/stock_transfer_report.xml',
-        'report/stock_transfer_report_template.xml',
+        'report/stock_transfer_charge_report_action.xml',
+        'report/stock_transfer_charge_report_templates.xml',
     ],
     'installable': True,
     'application': False,
--- custom_addons/stock_transfer_charg/models/__init__.py
+++ custom_addons/stock_transfer_charg/models/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import report_stock_transfer
+from . import report_stock_transfer_charge
