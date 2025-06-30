7f14d4d8bf13bd467c9a27c31758bfb4458697df
unknown <dante.h@hotmail.es>
2025-06-30 11:03:03 -0400

    cambios modelo_charger10

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -4,28 +4,28 @@ from datetime import datetime
 
 class ReportStockTransferCharge(models.AbstractModel):
     _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
-    _description = 'Reporte de Cargos entre Locales'
+    _description = 'Reporte mensual de cargos entre locales'
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        data_by_month = defaultdict(list)
-
         pickings = self.env['stock.picking'].search([
             ('picking_type_code', '=', 'internal'),
             ('state', '=', 'done'),
             ('date_done', '!=', False),
         ])
 
+        data_by_month = defaultdict(list)
+
         for picking in pickings:
             date_done = picking.date_done
-            month = date_done.strftime('%Y-%m')
+            mes = date_done.strftime('%Y-%m')
 
             for move in picking.move_ids_without_package:
                 price = move.product_id.standard_price or 0.0
                 qty = move.quantity_done or 0.0
-                total = qty * price
+                total = price * qty
 
-                data_by_month[month].append({
+                data_by_month[mes].append({
                     'date': date_done.strftime('%d/%m/%Y'),
                     'origin': picking.location_id.display_name,
                     'destination': picking.location_dest_id.display_name,
@@ -35,6 +35,9 @@ class ReportStockTransferCharge(models.AbstractModel):
                     'total': total,
                 })
 
+        # 🔥 Esto asegura que el template reciba la variable que busca
         return {
-            'data_by_month': dict(data_by_month),  # 👈🏼 ESTA VARIABLE ES LA CLAVE
+            'doc_ids': docids,
+            'doc_model': 'stock.picking',
+            'data_by_month': dict(data_by_month),
         }
