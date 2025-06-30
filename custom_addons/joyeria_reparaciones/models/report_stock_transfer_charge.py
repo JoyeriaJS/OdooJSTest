a8097c2c8d2a60b1751f421a99bb751d76eb58cb
unknown <dante.h@hotmail.es>
2025-06-30 13:53:18 -0400

    cambios modelo_charger28

---
+++ custom_addons/joyeria_reparaciones/models/report_stock_transfer_charge.py
@@ -0,0 +1,42 @@
+from odoo import models, api
+from collections import defaultdict
+from datetime import datetime
+
+class ReportStockTransferCharge(models.AbstractModel):
+    _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
+    _description = 'Reporte de Cargos entre Locales por Traspasos'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        pickings = self.env['stock.picking'].search([
+            ('picking_type_code', '=', 'internal'),
+            ('state', '=', 'done')
+        ])
+
+        data_by_month = defaultdict(list)
+
+        for picking in pickings:
+            date_done = picking.date_done or picking.scheduled_date
+            if not date_done:
+                continue
+            month_key = date_done.strftime('%B %Y')
+            origin = picking.location_id.display_name
+            destination = picking.location_dest_id.display_name
+
+            for move in picking.move_ids.filtered(lambda m: m.state == 'done'):
+                qty = move.quantity_done
+                price_unit = move.product_id.standard_price
+                subtotal = qty * price_unit
+
+                data_by_month[month_key].append({
+                    'origin': origin,
+                    'destination': destination,
+                    'product': move.product_id.display_name,
+                    'quantity': qty,
+                    'price_unit': price_unit,
+                    'subtotal': subtotal,
+                })
+
+        return {
+            'data_by_month': dict(data_by_month)
+        }
