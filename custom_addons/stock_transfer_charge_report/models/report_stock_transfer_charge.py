0729ac5f0c4148966a6676fda9d205bc6297d103
unknown <dante.h@hotmail.es>
2025-06-30 11:12:18 -0400

    cambios modelo_charger11

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -4,40 +4,32 @@ from datetime import datetime
 
 class ReportStockTransferCharge(models.AbstractModel):
     _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
-    _description = 'Reporte mensual de cargos entre locales'
+    _description = 'Reporte de Cargos entre Locales por Traspasos'
 
     @api.model
     def _get_report_values(self, docids, data=None):
         pickings = self.env['stock.picking'].search([
             ('picking_type_code', '=', 'internal'),
             ('state', '=', 'done'),
-            ('date_done', '!=', False),
+            ('date_done', '!=', False)
         ])
 
         data_by_month = defaultdict(list)
-
         for picking in pickings:
-            date_done = picking.date_done
-            mes = date_done.strftime('%Y-%m')
-
-            for move in picking.move_ids_without_package:
-                price = move.product_id.standard_price or 0.0
-                qty = move.quantity_done or 0.0
-                total = price * qty
-
-                data_by_month[mes].append({
-                    'date': date_done.strftime('%d/%m/%Y'),
+            month = picking.date_done.strftime('%Y-%m')
+            for move in picking.move_lines:
+                qty = move.quantity_done
+                price = move.product_id.standard_price
+                total = qty * price
+                data_by_month[month].append({
                     'origin': picking.location_id.display_name,
                     'destination': picking.location_dest_id.display_name,
                     'product': move.product_id.display_name,
-                    'quantity': qty,
+                    'qty': qty,
                     'price': price,
                     'total': total,
                 })
 
-        # 🔥 Esto asegura que el template reciba la variable que busca
         return {
-            'doc_ids': docids,
-            'doc_model': 'stock.picking',
             'data_by_month': dict(data_by_month),
         }
