97f4c51a22f31cb7c9620f9a8efda19e00099d8f
unknown <dante.h@hotmail.es>
2025-06-30 12:16:02 -0400

    cambios modelo_charger15

--- custom_addons/stock_transfer_charge_report/__manifest__.py
+++ custom_addons/stock_transfer_charge_report/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': "Cargos entre Locales por Traspasos",
     'version': '1.0',
-    'depends': ['stock', 'report'],
+    'depends': ['stock', 'base', 'product'],
     'author': "Tu Empresa",
     'category': 'Inventory',
     'description': 'Reporte de cargos entre locales a través de traspasos internos',
