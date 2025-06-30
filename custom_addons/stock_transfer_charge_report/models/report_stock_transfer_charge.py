e6d5d4cf7ca9a28fb4459a18ac30e26af29ee4af
unknown <dante.h@hotmail.es>
2025-06-30 10:19:57 -0400

    cambios modelo_charger5

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -11,6 +11,7 @@ class ReportMonthlyTransferCharges(models.AbstractModel):
         moves = self.env['stock.move'].search([
             ('picking_id.picking_type_code', '=', 'internal'),
             ('state', '=', 'done'),
+            ('quantity_done', '>', 0),
         ])
 
         data_by_month = defaultdict(list)
@@ -23,15 +24,17 @@ class ReportMonthlyTransferCharges(models.AbstractModel):
             fecha = fields.Datetime.context_timestamp(self, picking.date_done)
             mes = fecha.strftime('%B %Y')
 
+            total = move.quantity_done * (move.product_id.standard_price or 0.0)
+
             data_by_month[mes].append({
                 'origin': move.location_id.display_name or '',
                 'destination': move.location_dest_id.display_name or '',
                 'product': move.product_id.display_name or '',
-                'total': (move.quantity_done or 0.0) * (move.product_id.standard_price or 0.0),
+                'total': total,
             })
 
         return {
             'doc_ids': docids,
             'doc_model': 'stock.move',
-            'data_by_month': dict(data_by_month),  # <--- Asegúrate que exista
+            'data_by_month': dict(data_by_month),
         }
