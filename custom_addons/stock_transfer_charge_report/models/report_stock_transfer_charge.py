7afeafd9eeec8938f6b4073df4ca43165aa558d2
unknown <dante.h@hotmail.es>
2025-06-30 10:45:17 -0400

    cambios modelo_charger9

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -1,40 +1,37 @@
-from odoo import models, api
+from odoo import models, fields, api
 from collections import defaultdict
 from datetime import datetime
 
 class ReportStockTransferCharge(models.AbstractModel):
     _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
-    _description = 'Reporte de cargos entre locales'
+    _description = 'Reporte de cargos entre locales agrupado por mes'
 
     @api.model
     def _get_report_values(self, docids, data=None):
         pickings = self.env['stock.picking'].search([
             ('picking_type_code', '=', 'internal'),
             ('state', '=', 'done'),
-            ('date_done', '!=', False),
         ])
 
-        grouped = defaultdict(list)
-
+        data_by_month = defaultdict(list)
         for picking in pickings:
-            fecha = picking.date_done
-            year_month = fecha.strftime('%Y-%m')
-
-            for move in picking.move_lines:
-                qty = move.quantity_done or 0.0
-                price = move.product_id.standard_price or 0.0
-                total = qty * price
-
-                grouped[year_month].append({
-                    'date': fecha.strftime('%d/%m/%Y'),
+            date = picking.date_done
+            if not date:
+                continue
+            month_key = date.strftime('%Y-%m')
+            for move in picking.move_ids_without_package:
+                data_by_month[month_key].append({
+                    'date': date.strftime('%d/%m/%Y'),
                     'origin': picking.location_id.display_name,
                     'destination': picking.location_dest_id.display_name,
                     'product': move.product_id.display_name,
-                    'qty': qty,
-                    'unit_price': price,
-                    'total': total,
+                    'quantity': move.quantity_done,
+                    'price': move.product_id.standard_price,
+                    'total': move.quantity_done * move.product_id.standard_price,
                 })
 
+        # Ordenamos por mes
+        sorted_data = dict(sorted(data_by_month.items()))
         return {
-            'data_by_month': dict(grouped),
+            'data_by_month': sorted_data,
         }
