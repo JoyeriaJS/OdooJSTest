ba4157e5db2beae5210b3f15846dea1350ee79f3
unknown <dante.h@hotmail.es>
2025-07-01 13:02:41 -0400

    cambios reporte de transferencia37

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -8,9 +8,7 @@ class ReportDeliverySlipCharge(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # Llamamos al padre para mantener todo lo original
         res = super(ReportDeliverySlipCharge, self)._get_report_values(docids, data)
-        # Filtramos solo transfers internos completados
         pickings = self.env['stock.picking'].browse(docids).filtered(
             lambda p: p.picking_type_code == 'internal' and p.state == 'done'
         )
@@ -26,7 +24,6 @@ class ReportDeliverySlipCharge(models.AbstractModel):
             dest = pick.location_dest_id.display_name
             for mv in pick.move_ids.filtered(lambda m: m.state == 'done' and m.product_uom_qty):
                 grouped[(month, origin, dest)] += mv.product_uom_qty * (mv.product_id.standard_price or 0.0)
-        # Construimos lista ordenada
         res['charge_lines'] = [
             {'month': m, 'origin': o, 'dest': d, 'amount': amt}
             for (m, o, d), amt in sorted(grouped.items())
