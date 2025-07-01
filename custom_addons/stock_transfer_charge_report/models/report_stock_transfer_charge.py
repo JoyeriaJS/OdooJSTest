1177cac3ae09fc5829d2e0650004795028df08f9
unknown <dante.h@hotmail.es>
2025-07-01 17:17:26 -0400

    cambios reporte de transferencia71

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -2,32 +2,26 @@ from odoo import models, api
 
 class ReportStockTransferCharge(models.AbstractModel):
     _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
-    _description = 'Reporte de Cargos entre Locales por Traspasos'
+    _description = 'Reporte General de Traspasos'
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # Si docids está vacío, trae todos los pickings (para prueba)
-        if not docids:
-            pickings = self.env['stock.picking'].search([])
-        else:
-            pickings = self.env['stock.picking'].browse(docids)
-        
+        # Buscar todos los pickings (sin filtro, para probar)
+        pickings = self.env['stock.picking'].search([])
         lines = []
         for picking in pickings:
-            for move in picking.move_ids:
+            for move in picking.move_ids:  # stock.move, NO move_line_ids
                 lines.append({
-                    'picking_name': picking.name or '',
-                    'origin': picking.location_id.display_name or '',
-                    'destination': picking.location_dest_id.display_name or '',
-                    'product': move.product_id.display_name or '',
-                    'quantity': move.product_uom_qty or 0,
-                    'price_unit': move.product_id.standard_price or 0,
-                    'subtotal': (move.product_uom_qty or 0) * (move.product_id.standard_price or 0),
-                    'state': picking.state or '',
-                    'type': picking.picking_type_id.name or '',
+                    'picking_name': picking.name,
+                    'origin': picking.location_id.display_name,
+                    'destination': picking.location_dest_id.display_name,
+                    'product': move.product_id.display_name,
+                    'quantity': move.product_uom_qty,
+                    'price_unit': move.product_id.standard_price,
+                    'subtotal': move.product_uom_qty * (move.product_id.standard_price or 0.0),
+                    'state': picking.state,
+                    'type': picking.picking_type_code,
                 })
-
-        return {
-            'lines': lines,
-            'test_text': f'IDs recibidos: {docids} - total traspasos: {len(lines)}'
-        }
+        # Si quieres ver por consola lo que obtiene:
+        # import logging; logging.getLogger('odoo').info(lines)
+        return {'lines': lines}
