41f0339033840db8c916f5e5e1eb0213e68bffc0
unknown <dante.h@hotmail.es>
2025-07-01 17:38:53 -0400

    cambios reporte de transferencia72

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -6,22 +6,10 @@ class ReportStockTransferCharge(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # Buscar todos los pickings (sin filtro, para probar)
-        pickings = self.env['stock.picking'].search([])
-        lines = []
-        for picking in pickings:
-            for move in picking.move_ids:  # stock.move, NO move_line_ids
-                lines.append({
-                    'picking_name': picking.name,
-                    'origin': picking.location_id.display_name,
-                    'destination': picking.location_dest_id.display_name,
-                    'product': move.product_id.display_name,
-                    'quantity': move.product_uom_qty,
-                    'price_unit': move.product_id.standard_price,
-                    'subtotal': move.product_uom_qty * (move.product_id.standard_price or 0.0),
-                    'state': picking.state,
-                    'type': picking.picking_type_code,
-                })
-        # Si quieres ver por consola lo que obtiene:
-        # import logging; logging.getLogger('odoo').info(lines)
-        return {'lines': lines}
+        # Toma los pickings seleccionados (o todos, para prueba)
+        pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
+        return {
+            'doc_ids': pickings.ids,
+            'doc_model': 'stock.picking',
+            'docs': pickings,
+        }
