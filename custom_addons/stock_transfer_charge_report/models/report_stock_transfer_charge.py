fc2ae942344cc049c59d482f2474efec99d9cc29
unknown <dante.h@hotmail.es>
2025-07-01 14:21:33 -0400

    cambios reporte de transferencia48

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -3,29 +3,40 @@ from odoo import api, models
 from collections import defaultdict
 import calendar
 
-class ReportDeliverySlipCharge(models.AbstractModel):
-    _inherit = 'report.stock.report_deliveryslip'
+class ReportStockTransferCharge(models.AbstractModel):
+    _name = 'report.stock_transfer_charge_report.report_stock_transfer_charge'
+    _description = 'Cargos entre Locales (Agrupado Mensual)'
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        res = super(ReportDeliverySlipCharge, self)._get_report_values(docids, data)
-        pickings = self.env['stock.picking'].browse(docids).filtered(
+        # docids vendrá de los stock.picking seleccionados en el wizard de impresión
+        Picking = self.env['stock.picking']
+        # solo internal y done
+        pickings = Picking.browse(docids).filtered(
             lambda p: p.picking_type_code == 'internal' and p.state == 'done'
         )
         grouped = defaultdict(float)
         for pick in pickings:
             if pick.date_done:
                 year = pick.date_done.year
-                month_num = pick.date_done.month
-                month = f"{calendar.month_name[month_num]} {year}"
+                month = calendar.month_name[pick.date_done.month]
+                label = f"{month} {year}"
             else:
-                month = 'Sin Fecha'
+                label = 'Sin Fecha'
             origin = pick.location_id.display_name
             dest = pick.location_dest_id.display_name
             for mv in pick.move_ids.filtered(lambda m: m.state == 'done' and m.product_uom_qty):
-                grouped[(month, origin, dest)] += mv.product_uom_qty * (mv.product_id.standard_price or 0.0)
-        res['charge_lines'] = [
-            {'month': m, 'origin': o, 'dest': d, 'amount': amt}
-            for (m, o, d), amt in sorted(grouped.items())
+                grouped[(label, origin, dest)] += mv.product_uom_qty * (mv.product_id.standard_price or 0.0)
+
+        charge_lines = [
+            {'month': mon, 'origin': ori, 'dest': dst, 'amount': amt}
+            for (mon, ori, dst), amt in sorted(grouped.items())
         ]
-        return res
+
+        return {
+            'doc_ids': docids,
+            'doc_model': 'stock.picking',
+            'data': data,
+            'docs': pickings,
+            'charge_lines': charge_lines,
+        }
