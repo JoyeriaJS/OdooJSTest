f6ec64af98b81da0c9bb6aef58a44b5667fe5382
unknown <dante.h@hotmail.es>
2025-07-01 16:02:47 -0400

    cambios reporte de transferencia63

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from odoo import api, models, fields
+from odoo import models, api
 from collections import defaultdict
+from datetime import datetime
 
 class ReportStockTransferCharge(models.AbstractModel):
     _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
@@ -8,35 +9,34 @@ class ReportStockTransferCharge(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # Sólo internal y done
-        pickings = self.env['stock.picking'].search([
-            ('picking_type_code', '=', 'internal'),
-            ('state', '=', 'done'),
-        ], order='date_done')
-
+        # Sólo tomamos las pickings seleccionadas y que sean internas y hechas
+        pickings = self.env['stock.picking'].browse(docids).filtered(
+            lambda p: p.picking_type_code == 'internal' and p.state == 'done'
+        )
         data_by_month = defaultdict(list)
-        for pick in pickings:
-            date = pick.date_done or pick.scheduled_date or pick.date or fields.Datetime.now()
-            month = fields.Datetime.context_timestamp(self, pick, date).strftime('%B %Y')
-            for ml in pick.move_line_ids.filtered(lambda l: l.qty_done):
-                qty   = ml.qty_done
-                pu    = ml.product_id.standard_price or 0.0
-                data_by_month[month].append({
-                    'origin':      pick.location_id.display_name,
-                    'destination': pick.location_dest_id.display_name,
-                    'product':     ml.product_id.display_name,
-                    'quantity':    qty,
-                    'price_unit':  pu,
-                    'subtotal':    qty * pu,
+        for picking in pickings:
+            # Fecha para agrupar por mes
+            date = picking.date_done or picking.scheduled_date or picking.date or datetime.now()
+            month_key = date.strftime('%B %Y')
+            for move in picking.move_ids.filtered(lambda m: m.state=='done' and m.product_uom_qty):
+                qty = move.product_uom_qty
+                price = move.product_id.standard_price or 0.0
+                subtotal = qty * price
+                data_by_month[month_key].append({
+                    'origin': picking.location_id.display_name,
+                    'destination': picking.location_dest_id.display_name,
+                    'product': move.product_id.display_name,
+                    'quantity': qty,
+                    'price_unit': price,
+                    'subtotal': subtotal,
                 })
-
-        # Convertir a lista ordenada
+        # Construimos la lista final
         months = [
             {'month': m, 'lines': lines}
-            for m, lines in data_by_month.items()
+            for m, lines in sorted(data_by_month.items())
         ]
         return {
-            'doc_ids':     pickings.ids,
-            'doc_model':   'stock.picking',
-            'months':      months,
+            'doc_ids': docids,
+            'doc_model': 'stock.picking',
+            'months': months,
         }
