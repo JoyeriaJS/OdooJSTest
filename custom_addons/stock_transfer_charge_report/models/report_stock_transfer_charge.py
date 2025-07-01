98da4dd00f2b7ad57ed149f953058edd59b734f0
unknown <dante.h@hotmail.es>
2025-07-01 14:05:02 -0400

    cambios reporte de transferencia45

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -1,31 +1,36 @@
-# -*- coding: utf-8 -*-
-from odoo import api, models
-from collections import defaultdict
-import calendar
 
-class ReportDeliverySlipCharge(models.AbstractModel):
-    _inherit = 'report.stock.report_deliveryslip'
+from odoo import models, api
+from collections import defaultdict
+
+class ReportStockTransferCharge(models.AbstractModel):
+    _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
+    _description = 'Reporte de Cargos entre Locales por Traspasos'
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        res = super(ReportDeliverySlipCharge, self)._get_report_values(docids, data)
-        pickings = self.env['stock.picking'].browse(docids).filtered(
-            lambda p: p.picking_type_code == 'internal' and p.state == 'done'
-        )
-        grouped = defaultdict(float)
-        for pick in pickings:
-            if pick.date_done:
-                year = pick.date_done.year
-                month_num = pick.date_done.month
-                month = f"{calendar.month_name[month_num]} {year}"
-            else:
-                month = 'Sin Fecha'
-            origin = pick.location_id.display_name
-            dest = pick.location_dest_id.display_name
-            for mv in pick.move_ids.filtered(lambda m: m.state == 'done' and m.product_uom_qty):
-                grouped[(month, origin, dest)] += mv.product_uom_qty * (mv.product_id.standard_price or 0.0)
-        res['charge_lines'] = [
-            {'month': m, 'origin': o, 'dest': d, 'amount': amt}
-            for (m, o, d), amt in sorted(grouped.items())
-        ]
-        return res
+        pickings = self.env['stock.picking'].search([
+            ('picking_type_code', '=', 'internal'),
+            ('state', '=', 'done')
+        ])
+
+        data_by_month = defaultdict(list)
+
+        for picking in pickings:
+            if not picking.move_lines:
+                continue
+            if not picking.date_done:
+                continue
+            month_key = picking.date_done.strftime('%B %Y')
+            for move in picking.move_lines:
+                data_by_month[month_key].append({
+                    'origin': picking.location_id.display_name,
+                    'destination': picking.location_dest_id.display_name,
+                    'product': move.product_id.display_name,
+                    'quantity': move.quantity_done,
+                    'price_unit': move.product_id.standard_price,
+                    'subtotal': move.quantity_done * move.product_id.standard_price,
+                })
+
+        return {
+            'months': [{'month': m, 'lines': lines} for m, lines in data_by_month.items()]
+        }
