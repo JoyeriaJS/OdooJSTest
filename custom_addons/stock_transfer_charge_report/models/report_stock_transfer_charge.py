8fc310a6ed19da356993ebc7ef57085197c111dc
unknown <dante.h@hotmail.es>
2025-07-01 17:06:01 -0400

    cambios reporte de transferencia71

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -1,28 +1,33 @@
 from odoo import models, api
 
-class ReportPickingTransfer(models.AbstractModel):
+class ReportStockTransferCharge(models.AbstractModel):
     _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
-    _description = 'Reporte personalizado de traspasos'
+    _description = 'Reporte de Cargos entre Locales por Traspasos'
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # Solo los docids seleccionados
-        pickings = self.env['stock.picking'].browse(docids)
+        # Si docids está vacío, trae todos los pickings (para prueba)
+        if not docids:
+            pickings = self.env['stock.picking'].search([])
+        else:
+            pickings = self.env['stock.picking'].browse(docids)
+        
         lines = []
         for picking in pickings:
-            for move in picking.move_ids_without_package:
+            for move in picking.move_ids:
                 lines.append({
-                    'picking_name': picking.name,
-                    'origin': picking.location_id.display_name,
-                    'destination': picking.location_dest_id.display_name,
-                    'product': move.product_id.display_name,
-                    'quantity': move.product_uom_qty,
-                    'price_unit': move.product_id.standard_price,
-                    'subtotal': move.product_uom_qty * (move.product_id.standard_price or 0.0),
-                    'state': picking.state,
-                    'type': picking.picking_type_code,
+                    'picking_name': picking.name or '',
+                    'origin': picking.location_id.display_name or '',
+                    'destination': picking.location_dest_id.display_name or '',
+                    'product': move.product_id.display_name or '',
+                    'quantity': move.product_uom_qty or 0,
+                    'price_unit': move.product_id.standard_price or 0,
+                    'subtotal': (move.product_uom_qty or 0) * (move.product_id.standard_price or 0),
+                    'state': picking.state or '',
+                    'type': picking.picking_type_id.name or '',
                 })
+
         return {
             'lines': lines,
-            'test_text': "ESTE ES EL REPORTE DE TRASPASOS"  # Solo para probar
+            'test_text': f'IDs recibidos: {docids} - total traspasos: {len(lines)}'
         }
