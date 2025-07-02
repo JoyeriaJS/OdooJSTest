430403ccd19c0b148743a9eee53b3f9675980d73
unknown <dante.h@hotmail.es>
2025-07-02 09:58:33 -0400

    cambios reporte local 7

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -1,5 +1,4 @@
 from odoo import models, api
-from collections import defaultdict
 
 class ReportStockTransferCharge(models.AbstractModel):
     _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
@@ -7,32 +6,10 @@ class ReportStockTransferCharge(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
+        # Toma los pickings seleccionados (o todos, para prueba)
         pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
-
-        # Agrupar por mes y por destino (local que recibe)
-        resumen = defaultdict(lambda: defaultdict(float))
-        for picking in pickings:
-            fecha = picking.date_done
-            if not fecha:
-                continue
-            mes = fecha.strftime('%B %Y')  # Ejemplo: "July 2025"
-            destino = picking.location_dest_id.display_name
-            for ml in picking.move_line_ids_without_package:
-                subtotal = ml.quantity * (ml.product_id.standard_price or 0.0)
-                resumen[mes][destino] += subtotal
-
-        resumen_listo = []
-        for mes, destinos in sorted(resumen.items()):
-            for destino, total in destinos.items():
-                resumen_listo.append({
-                    'mes': mes,
-                    'destino': destino,
-                    'total': round(total, 2),
-                })
-
         return {
             'doc_ids': pickings.ids,
             'doc_model': 'stock.picking',
             'docs': pickings,
-            'resumen_mensual': resumen_listo,
         }
