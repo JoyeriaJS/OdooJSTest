11445e14048ab8698b49edc280fcb32ecd728bbd
unknown <dante.h@hotmail.es>
2025-07-03 15:33:03 -0400

    cambios reportes y productos42

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -7,38 +7,28 @@ class ReportStockTransferCharge(models.AbstractModel):
     @api.model
     def _get_report_values(self, docids, data=None):
         pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
-        movimientos = []
-
-        # Busca la lista de precios "Interno (CLP)" solo una vez
+        
+        # Buscar la lista de precios "Interno (CLP)"
         pricelist = self.env['product.pricelist'].search([('name', '=', 'Interno (CLP)')], limit=1)
+        precios_interno = {}
 
-        for picking in pickings:
-            for ml in picking.move_line_ids_without_package:
-                precio_interno = 0.0
-                if pricelist:
-                    # OJO: el campo product_tmpl_id es el que cruza con la lista de precios
-                    item = self.env['product.pricelist.item'].search([
-                        ('pricelist_id', '=', pricelist.id),
-                        ('product_tmpl_id', '=', ml.product_id.product_tmpl_id.id),
-                    ], limit=1)
-                    if item:
-                        precio_interno = item.fixed_price
-
-                movimientos.append({
-                    'producto': ml.product_id.display_name or '',
-                    'cantidad': ml.quantity or 0.0,
-                    'uom': ml.product_uom_id.name or '',
-                    'precio_interno': precio_interno,
-                    'subtotal': (ml.quantity or 0.0) * precio_interno,
-                    'origen': picking.location_id.display_name or '',
-                    'destino': picking.location_dest_id.display_name or '',
-                    'picking_name': picking.name or '',
-                    'fecha': picking.date_done.strftime('%d/%m/%Y %H:%M:%S') if picking.date_done else '',
-                    'estado': picking.state or '',
-                    'tipo': picking.picking_type_code or '',
-                })
+        if pricelist:
+            # Buscamos todos los productos involucrados en los pickings
+            productos = pickings.mapped('move_line_ids_without_package.product_id')
+            for producto in productos:
+                item = self.env['product.pricelist.item'].search([
+                    ('pricelist_id', '=', pricelist.id),
+                    ('product_tmpl_id', '=', producto.product_tmpl_id.id),
+                    ('applied_on', '=', '1_product')
+                ], limit=1)
+                precios_interno[producto.id] = item.fixed_price if item else 0.0
+        else:
+            # Si no hay pricelist, todos a 0
+            productos = pickings.mapped('move_line_ids_without_package.product_id')
+            for producto in productos:
+                precios_interno[producto.id] = 0.0
 
         return {
             'docs': pickings,
-            'movimientos': movimientos,
+            'precios_interno': precios_interno,  # <--- lo que usarás en el template
         }
