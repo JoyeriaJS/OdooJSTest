e7b2e87cbdf51f1ff5961ba1f99b288348f3cd1e
unknown <dante.h@hotmail.es>
2025-07-03 16:29:37 -0400

    cambios reportes y productos50

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -7,24 +7,26 @@ class ReportStockTransferCharge(models.AbstractModel):
     @api.model
     def _get_report_values(self, docids, data=None):
         pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
-
         pricelist = self.env['product.pricelist'].search([('name', '=', 'Interno (CLP)')], limit=1)
         precios_interno = {}
+
         if pricelist:
+            # Recorre todos los productos únicos en los traspasos
             productos = pickings.mapped('move_line_ids_without_package.product_id')
             for producto in productos:
                 item = self.env['product.pricelist.item'].search([
                     ('pricelist_id', '=', pricelist.id),
                     ('product_tmpl_id', '=', producto.product_tmpl_id.id),
-                    ('applied_on', '=', '1_product')
+                    ('applied_on', '=', '1_product'),
                 ], limit=1)
                 precios_interno[producto.id] = item.fixed_price if item else 0.0
         else:
+            # Si no hay tarifa, todos los productos a 0
             productos = pickings.mapped('move_line_ids_without_package.product_id')
             for producto in productos:
                 precios_interno[producto.id] = 0.0
 
         return {
             'docs': pickings,
-            'precios_interno': precios_interno,  # <-- SIEMPRE mandamos este dict
+            'precios_interno': precios_interno,
         }
