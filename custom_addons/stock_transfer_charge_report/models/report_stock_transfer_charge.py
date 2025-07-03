bbfd409f0c8c27c82e7c551c68571de789e4b84c
unknown <dante.h@hotmail.es>
2025-07-03 11:13:25 -0400

    cambios reportes y productos26

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -8,10 +8,8 @@ class ReportStockTransferCharge(models.AbstractModel):
     @api.model
     def _get_report_values(self, docids, data=None):
         pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
-        
-        pricelist = self.env['product.pricelist'].search([('name', 'ilike', 'Interno')], limit=1)
         traspasos_por_mes = defaultdict(list)
-
+        pricelist = self.env['product.pricelist'].search([('name', 'ilike', 'Interno')], limit=1)
         for picking in pickings:
             mes = picking.date_done.strftime('%B %Y') if picking.date_done else 'Sin Fecha'
             for ml in picking.move_line_ids_without_package:
@@ -26,22 +24,17 @@ class ReportStockTransferCharge(models.AbstractModel):
                         precio_interno = item.fixed_price
                 subtotal = ml.quantity * precio_interno
                 traspasos_por_mes[mes].append({
-                    'code': ml.product_id.default_code or '',
-                    'name': ml.product_id.display_name or '',
-                    'qty': ml.quantity or 0.0,
+                    'producto': ml.product_id.display_name or '',
+                    'cantidad': ml.quantity or 0.0,
                     'uom': ml.product_uom_id.name or '',
-                    'origen': picking.location_id.display_name or '',
-                    'destino': picking.location_dest_id.display_name or '',
-                    'picking_name': picking.name or '',
-                    'fecha': picking.date_done.strftime('%d/%m/%Y %H:%M:%S') if picking.date_done else '',
-                    'estado': picking.state or '',
                     'precio_interno': precio_interno,
                     'subtotal_interno': subtotal,
+                    'origen': picking.location_id.display_name or '',
+                    'destino': picking.location_dest_id.display_name or '',
+                    'traspaso': picking.name or '',
+                    'fecha': picking.date_done.strftime('%d/%m/%Y %H:%M:%S') if picking.date_done else '',
+                    'estado': picking.state or '',
                 })
-
-        # DEBUG: Esto imprime en el log de Odoo
-        # print("DEBUG traspasos_por_mes", traspasos_por_mes)
-
         return {
-            'traspasos_por_mes': traspasos_por_mes,
+            'traspasos_por_mes': dict(traspasos_por_mes),  # <- asegúrate que es dict
         }
