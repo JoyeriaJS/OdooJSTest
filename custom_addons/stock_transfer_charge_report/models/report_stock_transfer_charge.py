7562251d1d56d8859d80412ff1dd547b8050c71b
unknown <dante.h@hotmail.es>
2025-07-03 11:34:28 -0400

    cambios reportes y productos32

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -2,22 +2,21 @@ from odoo import models, api
 
 class ReportStockTransferCharge(models.AbstractModel):
     _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
-    _description = 'Reporte Simple de Traspasos'
+    _description = 'Reporte Simple de Traspasos con Precio Interno'
 
     @api.model
     def _get_report_values(self, docids, data=None):
         pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
+
+        # Busca la lista de precios "Interno (CLP)"
+        pricelist = self.env['product.pricelist'].search([('name', '=', 'Interno (CLP)')], limit=1)
+
         movimientos = []
-
-        # Cargar la lista de precios "Interno (CLP)"
-        pricelist = self.env['product.pricelist'].search([('name', 'ilike', 'Interno')], limit=1)
-
         for picking in pickings:
             for ml in picking.move_line_ids_without_package:
-                # Buscar el precio interno para el producto
+                # Busca el precio interno del producto, si existe en la lista "Interno (CLP)"
                 precio_interno = 0.0
                 if pricelist:
-                    # Hay veces que los productos están como producto o plantilla (ajusta según tu modelo si tienes variantes)
                     item = self.env['product.pricelist.item'].search([
                         ('pricelist_id', '=', pricelist.id),
                         ('product_tmpl_id', '=', ml.product_id.product_tmpl_id.id),
@@ -34,11 +33,13 @@ class ReportStockTransferCharge(models.AbstractModel):
                     'subtotal': (ml.quantity or 0.0) * precio_interno,
                     'origen': picking.location_id.display_name or '',
                     'destino': picking.location_dest_id.display_name or '',
-                    'traspaso': picking.name or '',
+                    'picking_name': picking.name or '',
                     'fecha': picking.date_done.strftime('%d/%m/%Y %H:%M:%S') if picking.date_done else '',
                     'estado': picking.state or '',
+                    'tipo': picking.picking_type_code or '',
                 })
 
         return {
+            'docs': pickings,
             'movimientos': movimientos,
         }
