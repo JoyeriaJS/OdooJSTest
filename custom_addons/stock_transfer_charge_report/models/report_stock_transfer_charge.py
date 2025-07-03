b4f8cc63448b0c65302b319b9608b24b5b2cc95c
unknown <dante.h@hotmail.es>
2025-07-03 15:13:21 -0400

    cambios reportes y productos38

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -7,28 +7,25 @@ class ReportStockTransferCharge(models.AbstractModel):
     @api.model
     def _get_report_values(self, docids, data=None):
         pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
+
         # Busca la lista de precios "Interno (CLP)"
         pricelist = self.env['product.pricelist'].search([('name', '=', 'Interno (CLP)')], limit=1)
         precios_interno = {}
-        for picking in pickings:
-            for ml in picking.move_line_ids_without_package:
-                product = ml.product_id
-                if not product:
-                    continue
-                if product.id in precios_interno:
-                    continue
-                precio_interno = 0.0
-                if pricelist:
+        if pricelist:
+            for picking in pickings:
+                for ml in picking.move_line_ids_without_package:
+                    product = ml.product_id
+                    if not product or product.id in precios_interno:
+                        continue
                     item = self.env['product.pricelist.item'].search([
                         ('pricelist_id', '=', pricelist.id),
                         ('product_tmpl_id', '=', product.product_tmpl_id.id),
                         ('applied_on', '=', '1_product'),
                     ], limit=1)
-                    if item:
-                        precio_interno = item.fixed_price
-                precios_interno[product.id] = precio_interno
+                    precio = item.fixed_price if item else 0.0
+                    precios_interno[product.id] = precio
 
         return {
             'docs': pickings,
-            'precios_interno': precios_interno,
+            'precios_interno': precios_interno,   # <- ESTE NOMBRE EXACTO
         }
