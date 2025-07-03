2c7601ea89502999fd19a5212f1f3448846cae64
unknown <dante.h@hotmail.es>
2025-07-03 10:46:22 -0400

    cambios reportes y productos20

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -1,63 +1,29 @@
 from odoo import models, api
-from collections import defaultdict
-from odoo.exceptions import AccessError
 
 class ReportStockTransferCharge(models.AbstractModel):
     _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
-    _description = 'Reporte General de Traspasos'
+    _description = 'Reporte Simple de Traspasos'
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        if not self.env.user.has_group('base.group_system'):
-            raise AccessError("Sólo los administradores pueden generar este reporte.")
-
         pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
-        pickings = pickings.filtered(lambda x: x.date_done)
+        movimientos = []
 
-        # Buscar la lista de precios "Interno (CLP)"
-        pricelist = self.env['product.pricelist'].search([('name', 'ilike', 'interno')], limit=1)
-        productos_precio_interno = {}
         for picking in pickings:
             for ml in picking.move_line_ids_without_package:
-                product = ml.product_id
-                if not product:
-                    continue
-                if product.id in productos_precio_interno:
-                    continue
-                precio_interno = 0.0
-                if pricelist:
-                    item = self.env['product.pricelist.item'].search([
-                        ('pricelist_id', '=', pricelist.id),
-                        ('product_tmpl_id', '=', product.product_tmpl_id.id),
-                        ('applied_on', '=', '1_product'),
-                    ], limit=1)
-                    if item:
-                        precio_interno = item.fixed_price
-                productos_precio_interno[product.id] = precio_interno
-
-        # Agrupación para el resumen mensual
-        resumen = defaultdict(lambda: defaultdict(float))
-        for picking in pickings:
-            mes = picking.date_done.strftime('%B %Y')
-            origen = picking.location_id.display_name
-            for ml in picking.move_line_ids_without_package:
-                precio_interno = productos_precio_interno.get(ml.product_id.id, 0.0)
-                subtotal = ml.quantity * precio_interno
-                resumen[mes][origen] += subtotal
-
-        resumen_listo = []
-        for mes, origenes in sorted(resumen.items()):
-            for origen, total in origenes.items():
-                resumen_listo.append({
-                    'mes': mes,
-                    'origen': origen,
-                    'total': round(total, 2),
+                movimientos.append({
+                    'code': ml.product_id.default_code or '',
+                    'name': ml.product_id.display_name or '',
+                    'qty': ml.quantity or 0.0,
+                    'uom': ml.product_uom_id.name or '',
+                    'origen': picking.location_id.display_name or '',
+                    'destino': picking.location_dest_id.display_name or '',
+                    'picking_name': picking.name or '',
+                    'fecha': picking.date_done.strftime('%d/%m/%Y %H:%M:%S') if picking.date_done else '',
+                    'estado': picking.state or '',
                 })
 
         return {
-            'doc_ids': pickings.ids,
-            'doc_model': 'stock.picking',
-            'docs': pickings,
-            'resumen_mensual': resumen_listo,
-            'precios_interno': productos_precio_interno,
+            'movimientos': movimientos,
+            'pickings': pickings,
         }
