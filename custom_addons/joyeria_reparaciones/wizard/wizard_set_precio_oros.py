0501e1ee42a4ac2eff3e803b45bece3f167da4dd
unknown <dante.h@hotmail.es>
2025-07-07 13:31:31 -0400

    cambios wizard oro9

--- custom_addons/joyeria_reparaciones/wizard/wizard_set_precio_oros.py
+++ custom_addons/joyeria_reparaciones/wizard/wizard_set_precio_oros.py
@@ -8,8 +8,7 @@ class WizardSetPrecioOros(models.TransientModel):
     precio_oro_rosado   = fields.Float("Oro Rosado 18k", required=True)
 
     def print_report(self):
-        return True
-        """ # Valores del wizard
+        # Valores del wizard
         data = {
             'precio_oro_amarillo': self.precio_oro_amarillo,
             'precio_oro_rosado': self.precio_oro_rosado,
@@ -18,4 +17,4 @@ class WizardSetPrecioOros(models.TransientModel):
         docids = self.env.context.get('active_ids', [])
         return self.env.ref('joyeria_reparaciones.action_report_sales_by_store').report_action(
             self.env['joyeria.reparacion'].browse(docids), data=data
-        ) """
+        )
