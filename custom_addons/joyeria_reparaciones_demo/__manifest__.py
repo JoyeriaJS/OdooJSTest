6747c9128b3304fc083c42a581a46cc52ea72e70
unknown <dante.h@hotmail.es>
2025-07-07 13:55:35 -0400

    cambios wizard oro11

---
+++ custom_addons/joyeria_reparaciones_demo/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Joyeria Reparaciones DEMO',
+    'version': '1.0',
+    'summary': 'Demo: Reporte PDF con Wizard y External ID',
+    'author': 'DR + ChatGPT',
+    'depends': ['base'],
+    'data': [
+        'models/reparacion.py',
+        'views/reparacion_views.xml',
+        'report/report_sales_by_store.xml',
+        'report/report_sales_by_store_template.xml',
+        'wizard/wizard_set_precio_oros_view.xml',
+        'wizard/wizard_set_precio_oros_action.xml',
+    ],
+    'application': True,
+}
