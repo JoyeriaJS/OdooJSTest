8550735aab061f1168326ec541f9a4c793678215
unknown <dante.h@hotmail.es>
2025-07-07 10:17:11 -0400

    cambios wizard oro6

--- custom_addons/joyeria_reparaciones/wizard/wizard_set_precio_oros.py
+++ custom_addons/joyeria_reparaciones/wizard/wizard_set_precio_oros.py
@@ -8,16 +8,13 @@ class WizardSetPrecioOros(models.TransientModel):
     precio_oro_rosado   = fields.Float("Oro Rosado 18k", required=True)
 
     def print_report(self):
-        # Esta función genera el reporte usando los valores ingresados
-        # Requiere que se le pase docids, por ejemplo con contexto o data.
+        # Valores del wizard
         data = {
             'precio_oro_amarillo': self.precio_oro_amarillo,
             'precio_oro_rosado': self.precio_oro_rosado,
         }
-        # Normalmente el wizard conoce los IDs a reportar a través del contexto
-        docids = self.env.context.get('inactive_ids', [])
-        return self.env.ref('joyeria_reparaciones.action_report_sales_by_store').report_action(docids, data=data)
-                             
-                             
-
-                             
+        # Obtener los IDs seleccionados (registros activos)
+        docids = self.env.context.get('active_ids', [])
+        return self.env.ref('joyeria_reparaciones.action_report_sales_by_store').report_action(
+            self.env['joyeria.reparacion'].browse(docids), data=data
+        )
