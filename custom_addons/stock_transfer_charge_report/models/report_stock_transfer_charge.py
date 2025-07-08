1e96ffcacdbbfe53c13058e9d4bc4a4d4d9625f0
unknown <dante.h@hotmail.es>
2025-07-08 10:05:22 -0400

    cambios reporte transferencias templates13

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -2,7 +2,7 @@ from odoo import models, api
 
 class ReportStockTransferCharge(models.AbstractModel):
     _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
-    _description = 'Reporte Simple de Traspasos con Precio Interno'
+    _description = 'Reporte Simple de Traspasos con Precio Unitario y Peso'
 
     @api.model
     def _get_report_values(self, docids, data=None):
@@ -16,27 +16,30 @@ class ReportStockTransferCharge(models.AbstractModel):
                 cantidad = ml.qty_done or ml.quantity or 0.0
                 precio_unitario = ml.product_id.standard_price or 0.0
                 subtotal = cantidad * precio_unitario
+                # asumimos product.weight en kg
                 peso = (ml.product_id.weight or 0.0) * cantidad
                 total_precio += subtotal
                 total_peso += peso
                 lines.append({
-                    'producto': ml.product_id.display_name,
-                    'cantidad': cantidad,
-                    'uom': ml.product_uom_id.name,
-                    'precio_unitario': precio_unitario,
-                    'subtotal': subtotal,
-                    'peso': peso,
+                    'producto':         ml.product_id.display_name,
+                    'cantidad':         cantidad,
+                    'uom':              ml.product_uom_id.name,
+                    'precio_unitario':  precio_unitario,
+                    'subtotal':         subtotal,
+                    'peso':             peso,
                 })
             res_docs.append({
-                'name': picking.name,
-                'location_id': picking.location_id.display_name,
-                'location_dest_id': picking.location_dest_id.display_name,
-                'state': picking.state,
-                'picking_type_code': picking.picking_type_code,
-                'lines': lines,
-                'total_precio': total_precio,
-                'total_peso': total_peso,
+                'name':               picking.name,
+                'location_id':        picking.location_id.display_name,
+                'location_dest_id':   picking.location_dest_id.display_name,
+                'state':              picking.state,
+                'picking_type_code':  picking.picking_type_code,
+                'lines':              lines,
+                'total_precio':       total_precio,
+                'total_peso':         total_peso,
             })
         return {
-            'docs': res_docs,
+            'doc_ids':   docids,
+            'doc_model': 'stock.picking',
+            'docs':      res_docs,
         }
