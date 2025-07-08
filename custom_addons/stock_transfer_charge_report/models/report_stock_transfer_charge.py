395eb9c5c58eb9c24f22032aee24015df5605560
unknown <dante.h@hotmail.es>
2025-07-08 09:32:53 -0400

    cambios reporte transferencias templates2

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -2,41 +2,50 @@ from odoo import models, api
 
 class ReportStockTransferCharge(models.AbstractModel):
     _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
-    _description = 'Reporte Simple de Traspasos con Precio Interno'
+    _description = 'Reporte de Traspasos con Peso y Firma'
 
     @api.model
     def _get_report_values(self, docids, data=None):
         pickings = self.env['stock.picking'].browse(docids)
-        res_docs = []
+        docs = []
+
         for picking in pickings:
-            total_precio = 0.0
             total_peso = 0.0
+            total_precio = 0.0
             lines = []
+
             for ml in picking.move_line_ids_without_package:
-                cantidad = ml.qty_done or ml.quantity or 0.0
-                precio_unitario = ml.product_id.standard_price or 0.0
+                producto = ml.product_id
+                # en Odoo 17 el stock.move.line registra qty_done
+                cantidad = ml.qty_done or 0.0
+                precio_unitario = producto.standard_price or 0.0
+                peso_unitario = producto.weight or 0.0  # en kg
+                peso_trasladado = peso_unitario * cantidad
                 subtotal = cantidad * precio_unitario
-                peso = (ml.product_id.weight or 0.0) * cantidad
+
+                total_peso += peso_trasladado
                 total_precio += subtotal
-                total_peso += peso
+
                 lines.append({
-                    'producto': ml.product_id.display_name,
+                    'producto': producto.display_name,
                     'cantidad': cantidad,
                     'uom': ml.product_uom_id.name,
                     'precio_unitario': precio_unitario,
+                    'peso': peso_trasladado,
                     'subtotal': subtotal,
-                    'peso': peso,
                 })
-            res_docs.append({
-                'name': picking.name,
-                'location_id': picking.location_id.display_name,
+
+            docs.append({
+                'name':             picking.name,
+                'location_id':      picking.location_id.display_name,
                 'location_dest_id': picking.location_dest_id.display_name,
-                'state': picking.state,
+                'state':            picking.state,
                 'picking_type_code': picking.picking_type_code,
-                'lines': lines,
-                'total_precio': total_precio,
-                'total_peso': total_peso,
+                'total_peso':       total_peso,
+                'total_precio':     total_precio,
+                'lines':            lines,
             })
+
         return {
-            'docs': res_docs,
+            'docs': docs,
         }
