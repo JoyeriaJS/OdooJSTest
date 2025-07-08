8af2f2b59f4bb98c608261a38170733fda741f99
unknown <dante.h@hotmail.es>
2025-07-08 11:08:42 -0400

    cambios reporte transferencias templates33

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -2,7 +2,7 @@ from odoo import models, api
 
 class ReportStockTransferCharge(models.AbstractModel):
     _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
-    _description = 'Reporte Simple de Traspasos con Precio Interno'
+    _description = 'Reporte Simple de Traspasos con Peso y Precio'
 
     @api.model
     def _get_report_values(self, docids, data=None):
@@ -24,8 +24,8 @@ class ReportStockTransferCharge(models.AbstractModel):
                     'cantidad': cantidad,
                     'uom': ml.product_uom_id.name,
                     'precio_unitario': precio_unitario,
-                    'subtotal': subtotal,
                     'peso': peso,
+                    'subtotal': subtotal,
                 })
             res_docs.append({
                 'name': picking.name,
