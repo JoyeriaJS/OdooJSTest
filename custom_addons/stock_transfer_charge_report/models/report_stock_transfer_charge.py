af2ae96eae20b031a39878c8333573d3c170010f
unknown <dante.h@hotmail.es>
2025-07-09 15:47:04 -0400

    cambios reporte a excel51

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -6,25 +6,20 @@ class ReportStockTransferCharge(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # 1) obtenemos los pickings
         pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
 
-        # 2) cargamos la pricelist "Interno (CLP)"
         pricelist = self.env['product.pricelist'].search([('name', '=', 'Interno (CLP)')], limit=1)
-
-        # 3) prepararemos un dict { move_line_id: precio_interno } y sumaremos el total
         precios_interno = {}
         total_interno = 0.0
 
         for picking in pickings:
             for ml in picking.move_line_ids_without_package:
                 qty = ml.quantity or 0.0
-                # buscamos el ítem de pricelist para este template
                 if pricelist:
                     item = self.env['product.pricelist.item'].search([
-                        ('pricelist_id',    '=', pricelist.id),
-                        ('product_tmpl_id',  '=', ml.product_id.product_tmpl_id.id),
-                        ('applied_on',       '=', '1_product'),
+                        ('pricelist_id',   '=', pricelist.id),
+                        ('product_tmpl_id', '=', ml.product_id.product_tmpl_id.id),
+                        ('applied_on',      '=', '1_product'),
                     ], limit=1)
                     precio_int = item.fixed_price if item else 0.0
                 else:
@@ -34,7 +29,9 @@ class ReportStockTransferCharge(models.AbstractModel):
                 total_interno += precio_int * qty
 
         return {
-            'docs':             pickings,
-            'precios_interno':  precios_interno,
-            'total_interno':    total_interno,
+            'doc_ids':         docids,
+            'doc_model':       'stock.picking',
+            'docs':            pickings,
+            'precios_interno': precios_interno,
+            'total_interno':   total_interno,
         }
