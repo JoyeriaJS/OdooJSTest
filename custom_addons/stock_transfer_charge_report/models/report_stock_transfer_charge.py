6ce85a75871b44afd91ae3dc50b95bada58830b0
unknown <dante.h@hotmail.es>
2025-07-09 15:41:44 -0400

    cambios reporte a excel50

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -6,49 +6,35 @@ class ReportStockTransferCharge(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # 1) Cargar pickings
+        # 1) obtenemos los pickings
         pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
 
-        # 2) Cargar la pricelist “Interno (CLP)”
+        # 2) cargamos la pricelist "Interno (CLP)"
         pricelist = self.env['product.pricelist'].search([('name', '=', 'Interno (CLP)')], limit=1)
 
-        movimientos = []
+        # 3) prepararemos un dict { move_line_id: precio_interno } y sumaremos el total
+        precios_interno = {}
         total_interno = 0.0
 
         for picking in pickings:
             for ml in picking.move_line_ids_without_package:
-                qty = ml.qty_done or ml.quantity or 0.0
-
-                # 3) Buscar el precio interno para este producto
+                qty = ml.quantity or 0.0
+                # buscamos el ítem de pricelist para este template
                 if pricelist:
                     item = self.env['product.pricelist.item'].search([
-                        ('pricelist_id', '=', pricelist.id),
-                        ('product_tmpl_id', '=', ml.product_id.product_tmpl_id.id),
-                        ('applied_on', '=', '1_product'),
+                        ('pricelist_id',    '=', pricelist.id),
+                        ('product_tmpl_id',  '=', ml.product_id.product_tmpl_id.id),
+                        ('applied_on',       '=', '1_product'),
                     ], limit=1)
                     precio_int = item.fixed_price if item else 0.0
                 else:
                     precio_int = 0.0
 
-                subtotal_int = qty * precio_int
-                total_interno += subtotal_int
-
-                movimientos.append({
-                    'code':              ml.product_id.default_code or '',
-                    'name':              ml.product_id.display_name or '',
-                    'qty':               qty,
-                    'uom':               ml.product_uom_id.name or '',
-                    'origen':            picking.location_id.display_name or '',
-                    'destino':           picking.location_dest_id.display_name or '',
-                    'picking_name':      picking.name or '',
-                    'fecha':             (picking.date_done.strftime('%d/%m/%Y %H:%M:%S')
-                                          if picking.date_done else ''),
-                    'estado':            picking.state or '',
-                    'precio_interno':    precio_int,
-                    'subtotal_interno':  subtotal_int,
-                })
+                precios_interno[ml.id] = precio_int
+                total_interno += precio_int * qty
 
         return {
-            'movimientos':    movimientos,
-            'total_interno':  total_interno,
+            'docs':             pickings,
+            'precios_interno':  precios_interno,
+            'total_interno':    total_interno,
         }
