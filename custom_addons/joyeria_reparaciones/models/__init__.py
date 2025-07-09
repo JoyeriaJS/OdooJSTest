4913a704b08f9d24e5d8403572f3f037ffe5a170
unknown <dante.h@hotmail.es>
2025-07-09 10:46:16 -0400

    cambios reporte a excel10

--- custom_addons/joyeria_reparaciones/models/__init__.py
+++ custom_addons/joyeria_reparaciones/models/__init__.py
@@ -1,6 +1,5 @@
 from . import reparacion
 from . import joyeria_producto  # este es el nuevo archivo
-from . import report_salida_taller_xlsx
 from . import wizard_reporte_responsables
 from . import reporte_salida_taller_wizard
 from . import report_sales_by_store
