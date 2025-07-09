6b3aa4bf6ee7604df9f6c0c8b72d5d784c91e848
unknown <dante.h@hotmail.es>
2025-07-09 17:56:35 -0400

    cambios reporte a excel80

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -1,30 +1,57 @@
+# -*- coding: utf-8 -*-
 from odoo import models, api
 
 class ReportStockTransferCharge(models.AbstractModel):
-    _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
-    _description = 'Reporte Simple de Traspasos'
+    _name = 'report.stock_transfer_charge'      # <- nombre corto
+    _description = 'Reporte Movimiento entre Locales'
+    _auto = False                               # <- no crear tabla en BD
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
-        movimientos = []
-
-        for picking in pickings:
-            for ml in picking.move_line_ids_without_package:
-                movimientos.append({
-                    'code': ml.product_id.default_code or '',
-                    'name': ml.product_id.display_name or '',
-                    'qty': ml.quantity or 0.0,
-                    'uom': ml.product_uom_id.name or '',
-                    'origen': picking.location_id.display_name or '',
-                    'destino': picking.location_dest_id.display_name or '',
-                    'picking_name': picking.name or '',
-                    'fecha': picking.date_done.strftime('%d/%m/%Y %H:%M:%S') if picking.date_done else '',
-                    'estado': picking.state or '',
+        # 1) recupero los pickings (traslados internos)
+        pickings = (
+            self.env['stock.picking'].browse(docids)
+            if docids else
+            self.env['stock.picking'].search([])
+        )
+        # 2) busco la pricelist interna
+        pricelist = self.env['product.pricelist'].search(
+            [('name', '=', 'Interno (CLP)')], limit=1
+        )
+        docs = []
+        for p in pickings:
+            lines = []
+            total_int = 0.0
+            for ml in p.move_line_ids_without_package:
+                qty = ml.qty_done or ml.quantity or 0.0
+                # precio interno por item:
+                int_price = 0.0
+                if pricelist:
+                    item = self.env['product.pricelist.item'].search([
+                        ('pricelist_id', '=', pricelist.id),
+                        ('product_tmpl_id', '=', ml.product_id.product_tmpl_id.id),
+                        ('applied_on', '=', '1_product'),
+                    ], limit=1)
+                    int_price = item.fixed_price or 0.0
+                sub_int = qty * int_price
+                total_int += sub_int
+                lines.append({
+                    'product_code':   ml.product_id.default_code or '',
+                    'product_name':   ml.product_id.display_name,
+                    'qty':            qty,
+                    'uom':            ml.product_uom_id.name,
+                    'int_price':      int_price,
+                    'subtotal_int':   sub_int,
                 })
-
+            docs.append({
+                'name':            p.name,
+                'origin':          p.location_id.display_name,
+                'destination':     p.location_dest_id.display_name,
+                'state':           p.state,
+                'picking_type':    p.picking_type_code,
+                'lines':           lines,
+                'total_int':       total_int,
+            })
         return {
-            'movimientos': movimientos,
-            'pickings': pickings,
-            
+            'docs': docs,
         }
