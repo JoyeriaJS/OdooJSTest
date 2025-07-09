28e9c154e84313c323cc82ee16d883efc79f44de
unknown <dante.h@hotmail.es>
2025-07-09 16:23:07 -0400

    cambios reporte a excel61

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -7,31 +7,53 @@ class ReportStockTransferCharge(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # 1) Obtengo los pickings (documentos)
+        # 1) recupero los pickings
         pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
 
-        # 2) Busco tu lista de precios interna
-        pricelist = self.env['product.pricelist'].search([('name', '=', 'Interno (CLP)')], limit=1)
+        # 2) obtengo la lista de precios interna
+        pricelist = self.env['product.pricelist'].search([('name','=','Interno (CLP)')], limit=1)
 
-        # 3) Creo un diccionario ml.id -> precio interno
-        precios_interno = {}
-        # Recorro todas las líneas sin paquete de esos pickings
-        for ml in pickings.mapped('move_line_ids_without_package'):
-            price = 0.0
-            if pricelist:
-                item = self.env['product.pricelist.item'].search([
-                    ('pricelist_id', '=', pricelist.id),
-                    ('product_tmpl_id', '=', ml.product_id.product_tmpl_id.id),
-                    ('applied_on', '=', '1_product'),
-                ], limit=1)
-                price = item.fixed_price or 0.0
-            precios_interno[ml.id] = price
-
-        # 4) Devuelvo todo lo necesario al QWeb
+        docs = []
+        for p in pickings:
+            lines = []
+            total_unit = 0.0
+            total_int  = 0.0
+            # 3) recorro cada línea sin paquete
+            for ml in p.move_line_ids_without_package:
+                qty  = ml.qty_done or ml.quantity or 0.0
+                up   = ml.product_id.standard_price or 0.0
+                # busco el precio interno para este producto
+                int_price = 0.0
+                if pricelist:
+                    item = self.env['product.pricelist.item'].search([
+                        ('pricelist_id','=',pricelist.id),
+                        ('product_tmpl_id','=',ml.product_id.product_tmpl_id.id),
+                        ('applied_on','=','1_product'),
+                    ], limit=1)
+                    int_price = item.fixed_price or 0.0
+                sub_unit = qty * up
+                sub_int  = qty * int_price
+                total_unit += sub_unit
+                total_int  += sub_int
+                lines.append({
+                    'producto':      ml.product_id.display_name,
+                    'cantidad':      qty,
+                    'uom':           ml.product_uom_id.name,
+                    'price_unit':    up,
+                    'price_int':     int_price,
+                    'subtotal_unit': sub_unit,
+                    'subtotal_int':  sub_int,
+                })
+            docs.append({
+                'name':            p.name,
+                'location':        p.location_id.display_name,
+                'location_dest':   p.location_dest_id.display_name,
+                'state':           p.state,
+                'type':            p.picking_type_code,
+                'lines':           lines,
+                'total_unit':      total_unit,
+                'total_int':       total_int,
+            })
         return {
-            'doc_ids': docids,
-            'doc_model': 'stock.picking',
-            'docs': pickings,
-            'precios_interno': precios_interno,
-            
+            'docs': docs,
         }
