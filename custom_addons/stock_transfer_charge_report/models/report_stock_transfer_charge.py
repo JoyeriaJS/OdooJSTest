dcff00699c131e3e4f77ddb4a4b17b8124e009a7
unknown <dante.h@hotmail.es>
2025-07-11 10:32:51 -0400

    cambios reporte movimientos locales6

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# models/report_stock_transfer_charge.py
 from odoo import models, api
 
 class ReportStockTransferCharge(models.AbstractModel):
@@ -7,32 +7,29 @@ class ReportStockTransferCharge(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # pickings
         pickings = self.env['stock.picking'].browse(docids) \
                    if docids else self.env['stock.picking'].search([])
 
-        # pricelist "Interno (CLP)"
+        # 1) Busca la pricelist "Interno (CLP)"
         pricelist = self.env['product.pricelist'].search(
             [('name','=','Interno (CLP)')], limit=1)
 
-        # arma mapa product_id → precio_interno
-        precios = {}
+        # 2) Construye el diccionario product_id → precio interno
+        precios_interno = {}
         for prod in pickings.mapped('move_line_ids_without_package.product_id'):
-            precios[prod.id] = 0.0
+            precios_interno[prod.id] = 0.0
             if pricelist:
                 item = self.env['product.pricelist.item'].search([
                     ('pricelist_id','=',pricelist.id),
                     ('product_tmpl_id','=',prod.product_tmpl_id.id),
                     ('applied_on','=','1_product'),
                 ], limit=1)
-                precios[prod.id] = item.fixed_price or 0.0
+                precios_interno[prod.id] = item.fixed_price or 0.0
 
         return {
             'doc_ids':         docids,
             'doc_model':       'stock.picking',
             'docs':            pickings,
-            # metemos el diccionario dentro de data
-            'data': {
-                'precios_interno': precios
-            },
+            # **Aquí**, metemos el mapa de precios al nivel raíz
+            'precios_interno': precios_interno,
         }
