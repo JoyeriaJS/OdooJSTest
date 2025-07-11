d30486ed53493ac09007678f840a20bf89adfa46
unknown <dante.h@hotmail.es>
2025-07-11 10:57:39 -0400

    cambios reporte movimientos locales8

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -7,7 +7,7 @@ class ReportStockTransferCharge(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # 1) Recuperar los pickings a mostrar
+        # 1) Recuperar pickings
         pickings = self.env['stock.picking'].browse(docids) \
             if docids else self.env['stock.picking'].search([])
 
@@ -15,19 +15,26 @@ class ReportStockTransferCharge(models.AbstractModel):
         pricelist = self.env['product.pricelist'].search(
             [('name', '=', 'Interno (CLP)')], limit=1)
 
-        # 3) Construir el diccionario {product_id: precio_interno}
+        # 3) Construir el diccionario product_id → precio interno
         precios_interno = {}
+        # Inicializo a 0 para todos los productos que aparezcan
         for prod in pickings.mapped('move_line_ids_without_package.product_id'):
             precios_interno[prod.id] = 0.0
-            if pricelist:
-                item = self.env['product.pricelist.item'].search([
-                    ('pricelist_id', '=', pricelist.id),
-                    ('product_tmpl_id', '=', prod.product_tmpl_id.id),
-                    ('applied_on', '=', '1_product'),
-                ], limit=1)
-                precios_interno[prod.id] = item.fixed_price or 0.0
+        # Si existe pricelist, leo cada ítem
+        if pricelist:
+            items = self.env['product.pricelist.item'].search([
+                ('pricelist_id', '=', pricelist.id),
+                ('applied_on', '=', '1_product'),
+                ('product_tmpl_id', 'in',
+                    pickings.mapped('move_line_ids_without_package.product_id.product_tmpl_id.id'))
+            ])
+            for it in items:
+                # Precio fijo por template
+                for prod in pickings.mapped('move_line_ids_without_package.product_id'):
+                    if prod.product_tmpl_id.id == it.product_tmpl_id.id:
+                        precios_interno[prod.id] = it.fixed_price or 0.0
 
-        # 4) Devolver NO sólo los pickings, sino también el mapa de precios
+        # 4) Devolver TODO lo necesario:
         return {
             'doc_ids':         pickings.ids,
             'doc_model':       'stock.picking',
