7ee29788efa714684fd64d6e57d8c78ccd483b2b
unknown <dante.h@hotmail.es>
2025-07-11 11:05:59 -0400

    cambios reporte movimientos locales10

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -4,37 +4,43 @@ from odoo import api, models
 class ReportStockTransferCharge(models.AbstractModel):
     _name = 'report.stock_transfer_charge_report.stock_transfer_charge_report_template'
     _description = 'Reporte Simple de Traspasos'
+    _auto = False   # evita que intente crear una tabla
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # 1) Recuperar pickings
+        # 1) los pickings
         pickings = self.env['stock.picking'].browse(docids) \
             if docids else self.env['stock.picking'].search([])
 
-        # 2) Cargar la pricelist “Interno (CLP)”
+        # 2) la pricelist “Interno (CLP)”
         pricelist = self.env['product.pricelist'].search(
             [('name', '=', 'Interno (CLP)')], limit=1)
 
-        # 3) Construir el diccionario product_id → precio interno
+        # 3) armo un dict: variant_id -> precio interno
         precios_interno = {}
-        # Inicializo a 0 para todos los productos que aparezcan
+        # inicializo en 0 para cada variante
         for prod in pickings.mapped('move_line_ids_without_package.product_id'):
             precios_interno[prod.id] = 0.0
-        # Si existe pricelist, leo cada ítem
+
+        # si existe pricelist, leo sus items (tanto aplicados a plantilla como a variante)
         if pricelist:
             items = self.env['product.pricelist.item'].search([
                 ('pricelist_id', '=', pricelist.id),
-                ('applied_on', '=', '1_product'),
-                ('product_tmpl_id', 'in',
-                    pickings.mapped('move_line_ids_without_package.product_id.product_tmpl_id.id'))
+                ('applied_on', 'in', ['0_product_variant', '1_product']),
+                '|',
+                  ('product_tmpl_id', 'in', pickings.mapped('move_line_ids_without_package.product_id.product_tmpl_id.id')),
+                  ('product_id',        'in', pickings.mapped('move_line_ids_without_package.product_id.id')),
             ])
             for it in items:
-                # Precio fijo por template
-                for prod in pickings.mapped('move_line_ids_without_package.product_id'):
-                    if prod.product_tmpl_id.id == it.product_tmpl_id.id:
-                        precios_interno[prod.id] = it.fixed_price or 0.0
+                if it.applied_on == '0_product_variant' and it.product_id:
+                    precios_interno[it.product_id.id] = it.fixed_price or 0.0
+                elif it.applied_on == '1_product' and it.product_tmpl_id:
+                    # todos los variantes de ese template
+                    for prod in pickings.mapped('move_line_ids_without_package.product_id'):
+                        if prod.product_tmpl_id.id == it.product_tmpl_id.id:
+                            precios_interno[prod.id] = it.fixed_price or 0.0
 
-        # 4) Devolver TODO lo necesario:
+        # 4) devolvemos TODO lo que la QWeb espera + nuestro dict
         return {
             'doc_ids':         pickings.ids,
             'doc_model':       'stock.picking',
