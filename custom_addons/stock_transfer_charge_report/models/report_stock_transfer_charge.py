a3ecfd98f48b991b6966f63dbd65c5e48e0d7a0e
unknown <dante.h@hotmail.es>
2025-07-11 13:34:33 -0400

    cambios reporte movimientos locales42

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -2,29 +2,29 @@
 from odoo import models, api
 
 class ReportStockTransferCharge(models.AbstractModel):
-    _name = 'report.stock_transfer_charge'
+    _name = 'report.mi_modulo.stock_transfer_charge'       # ← ESTE debe coincidir con el XML
     _description = 'Reporte Simple de Traspasos'
-    _auto = False  # evita que Odoo intente crear una tabla en la BD
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # 1) recupera los pickings seleccionados
-        pickings = self.env['stock.picking'].browse(docids or [])
+        # 1) recupera los pickings
+        pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
 
-        # 2) busca la tarifa "Interno (CLP)" y arma un dict {variant_id: precio_interno}
-        pricelist = self.env['product.pricelist'].search(
-            [('name', '=', 'Interno (CLP)')], limit=1)
+        # 2) carga la tarifa "Interno (CLP)" y construye un map { variante_id: precio_interno }
+        pricelist = self.env['product.pricelist'].search([('name', '=', 'Interno (CLP)')], limit=1)
         precios_interno = {}
         if pricelist:
             items = self.env['product.pricelist.item'].search([
                 ('pricelist_id', '=', pricelist.id),
-                ('applied_on',   '=', '1_product'),
+                ('applied_on',    '=', '1_product'),
             ])
             for item in items:
-                for v in item.product_tmpl_id.product_variant_ids:
-                    precios_interno[v.id] = item.fixed_price or 0.0
+                for var in item.product_tmpl_id.product_variant_ids:
+                    precios_interno[var.id] = item.fixed_price or 0.0
 
         return {
-            'docs':             pickings,
-            'precios_interno':  precios_interno,
+            'doc_ids':         pickings.ids,
+            'doc_model':       'stock.picking',
+            'docs':            pickings,
+            'precios_interno': precios_interno,
         }
