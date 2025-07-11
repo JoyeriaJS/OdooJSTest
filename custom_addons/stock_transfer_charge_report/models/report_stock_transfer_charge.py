76c92a0657508bd938afc052e0d6f5f7571a18fe
unknown <dante.h@hotmail.es>
2025-07-11 14:03:03 -0400

    cambios reporte movimientos locales50

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -2,28 +2,26 @@
 from odoo import api, models
 
 class StockTransferChargeReport(models.AbstractModel):
-    _name = 'report.stock_transfer_charge'
-    _description = 'Reporte Cargos entre Locales con Precio Interno'
+    _name = 'report.mi_modulo.stock_transfer_charge'
+    _description = 'Reporte de cargos entre locales'
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # 1) recogemos los pickings
         pickings = self.env['stock.picking'].browse(docids or [])
 
-        # 2) buscamos la pricelist “Interno (CLP)”
+        # 1) Buscamos la pricelist “Interno (CLP)”
         pricelist = self.env['product.pricelist'].search(
-            [('name', '=', 'Interno (CLP)')], limit=1)
-
-        # 3) construimos un dict { variant_id: fixed_price }
+            [('name', '=', 'Interno (CLP)')], limit=1
+        )
+        # 2) Preparamos el dict { variante_id: precio_interno }
         precios_interno = {}
         if pricelist:
-            items = self.env['product.pricelist.item'].search([
-                ('pricelist_id', '=', pricelist.id),
-                ('applied_on',     '=', '1_product'),
-            ])
-            for it in items:
-                for var in it.product_tmpl_id.product_variant_ids:
-                    precios_interno[var.id] = it.fixed_price or 0.0
+            for item in self.env['product.pricelist.item'].search([
+                    ('pricelist_id', '=', pricelist.id),
+                    ('applied_on',    '=', '1_product'),
+                ]):
+                for var in item.product_tmpl_id.product_variant_ids:
+                    precios_interno[var.id] = item.fixed_price or 0.0
 
         return {
             'doc_ids':         pickings.ids,
