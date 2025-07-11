127d2de145123921f60d2805f05a1f72c468fa3c
unknown <dante.h@hotmail.es>
2025-07-11 13:40:16 -0400

    cambios reporte movimientos locales46

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -2,7 +2,7 @@
 from odoo import models, api
 
 class ReportStockTransferCharge(models.AbstractModel):
-    _name = 'report.mi_modulo.stock_transfer_charge'       # ← ESTE debe coincidir con el XML
+    _name = 'report.stock_transfer_charge'
     _description = 'Reporte Simple de Traspasos'
 
     @api.model
@@ -10,9 +10,9 @@ class ReportStockTransferCharge(models.AbstractModel):
         # 1) recupera los pickings
         pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
 
-        # 2) carga la tarifa "Interno (CLP)" y construye un map { variante_id: precio_interno }
+        # 2) carga la tarifa "Interno (CLP)" y construye map { variante_id: precio_interno }
         pricelist = self.env['product.pricelist'].search([('name', '=', 'Interno (CLP)')], limit=1)
-        precios_interno = {}
+        precios_map = {}
         if pricelist:
             items = self.env['product.pricelist.item'].search([
                 ('pricelist_id', '=', pricelist.id),
@@ -20,11 +20,40 @@ class ReportStockTransferCharge(models.AbstractModel):
             ])
             for item in items:
                 for var in item.product_tmpl_id.product_variant_ids:
-                    precios_interno[var.id] = item.fixed_price or 0.0
+                    precios_map[var.id] = item.fixed_price or 0.0
 
+        # 3) construir lista de dicts
+        docs = []
+        for p in pickings:
+            lines = []
+            total_int = 0.0
+            for ml in p.move_line_ids_without_package:
+                qty    = ml.quantity or 0.0
+                unit   = ml.product_id.standard_price or 0.0
+                pint   = precios_map.get(ml.product_id.id, 0.0)
+                subt_u = qty * unit
+                subt_i = qty * pint
+                total_int += subt_i
+                lines.append({
+                    'producto':      ml.product_id.display_name,
+                    'cantidad':      qty,
+                    'uom':           ml.product_uom_id.name,
+                    'precio_unit':   unit,
+                    'precio_int':    pint,
+                    'subtotal_unit': subt_u,
+                    'subtotal_int':  subt_i,
+                })
+            docs.append({
+                'name':             p.name,
+                'location':         p.location_id.display_name,
+                'location_dest':    p.location_dest_id.display_name,
+                'state':            p.state,
+                'type':             p.picking_type_code,
+                'lines':            lines,
+                'total_int':        total_int,
+            })
         return {
-            'doc_ids':         pickings.ids,
-            'doc_model':       'stock.picking',
-            'docs':            pickings,
-            'precios_interno': precios_interno,
+            'doc_ids':    pickings.ids,
+            'doc_model':  'stock.picking',
+            'docs':       docs,
         }
