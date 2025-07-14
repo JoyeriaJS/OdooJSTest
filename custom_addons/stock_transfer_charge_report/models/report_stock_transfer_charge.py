ea7f0073026d748133b070c100cacf01633f4fa9
unknown <dante.h@hotmail.es>
2025-07-14 17:51:14 -0400

    cambios reporte cobro de taller a locales42

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -7,44 +7,29 @@ class StockTransferChargeReport(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # 1) Cargo los pickings
         pickings = self.env['stock.picking'].browse(docids or [])
 
-        # 2) Busco la pricelist "Interno (CLP)" (ajusta si tu empresa la llamó distinto)
+        # Pricelist “Interno (CLP)”
         pricelist = self.env['product.pricelist'].search(
             [('name', 'ilike', 'Interno')], limit=1
         )
 
-        # 3) Construyo un dict { variante_id: precio_fijo_interno }
         precios_interno = {}
-        precio_global = 0.0
         if pricelist:
-            items = self.env['product.pricelist.item'].search([
-                ('pricelist_id', '=', pricelist.id),
-                ('compute_price', '=', 'fixed'),
-            ])
-            for item in items:
-                # global
-                if item.applied_on == '3_global':
-                    precio_global = item.fixed_price or 0.0
-                # por categoría
-                elif item.applied_on == '2_product_category' and item.categ_id:
-                    for tmpl in item.categ_id.product_tmpl_ids:
-                        for var in tmpl.product_variant_ids:
-                            precios_interno[var.id] = item.fixed_price or 0.0
-                # por producto (plantilla)
-                elif item.applied_on == '1_product' and item.product_tmpl_id:
-                    for var in item.product_tmpl_id.product_variant_ids:
-                        precios_interno[var.id] = item.fixed_price or 0.0
-                # por variante
-                elif item.applied_on == '0_product_variant' and item.product_id:
-                    precios_interno[item.product_id.id] = item.fixed_price or 0.0
-
-        # 4) Devuelvo al QWeb
+            for picking in pickings:
+                for ml in picking.move_line_ids_without_package:
+                    # aquí forzamos qty=1.0 para obtener el precio unitario
+                    price_unit = pricelist.get_product_price(
+                        ml.product_id,
+                        1.0,
+                        ml.product_uom_id.id,
+                        picking.partner_id.id or False,
+                    )
+                    precios_interno[ml.id] = price_unit
+        # Si no hay pricelist, todas quedan en 0.0 por defecto
         return {
             'doc_ids':         pickings.ids,
             'doc_model':       'stock.picking',
             'docs':            pickings,
             'precios_interno': precios_interno,
-            'precio_global':   precio_global,
         }
