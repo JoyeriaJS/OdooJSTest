1b3840a5a45255abd33f18b537ad149f9300cc59
unknown <dante.h@hotmail.es>
2025-07-15 15:27:19 -0400

    cambios reporte traspaso productos15

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -4,36 +4,32 @@ from odoo import api, models
 
 class StockTransferChargeReport(models.AbstractModel):
     _name = 'report.mi_modulo.stock_transfer_charge'
-    _description = 'Reporte de cargos entre locales'
 
     @api.model
     def _get_report_values(self, docids, data=None):
         pickings = self.env['stock.picking'].browse(docids or [])
 
-        # 1) Recuperamos la pricelist "Interno (CLP)" (o que contenga 'Interno')
-        pricelist = self.env['product.pricelist'].search(
-            [('name', 'ilike', 'Interno')], limit=1
-        )
-
-        # 2) Calculamos el precio interno para cada move_line
+        # 1) Cargo la Tarifa “Interno” o “Interno (CLP)”
+        Tarifa = self.env['product.pricelist']
+        regla  = self.env['product.pricelist.item']
         precios_interno = {}
-        for picking in pickings:
-            partner_id = picking.partner_id.id or False
-            for ml in picking.move_line_ids_without_package:
-                qty    = ml.quantity or 0.0
-                uom_id = ml.product_uom_id.id
-                if pricelist:
-                    # get_product_price en Odoo 17 devuelve el precio fijo
-                    price = pricelist.get_product_price(
-                        ml.product_id, qty, uom_id, partner_id
-                    )
-                else:
-                    price = 0.0
-                precios_interno[ml.id] = price
+
+        tarifa = Tarifa.search([('name','ilike','Interno')], limit=1)
+        if tarifa:
+            # 2) Cojo todas las reglas de precio fijas
+            items = regla.search([
+                ('pricelist_id','=', tarifa.id),
+                ('applied_on',   '=', '1_product'),
+            ])
+            for item in items:
+                fp = item.fixed_price or 0.0
+                # Aplica a todas sus variantes:
+                for v in item.product_tmpl_id.product_variant_ids:
+                    precios_interno[v.id] = fp
 
         return {
-            'doc_ids':         pickings.ids,
             'doc_model':       'stock.picking',
+            'doc_ids':         pickings.ids,
             'docs':            pickings,
             'precios_interno': precios_interno,
         }
