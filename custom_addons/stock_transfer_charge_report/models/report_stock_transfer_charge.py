a76fe64b2ca73827f58e2036ecb84f2f0754c833
unknown <dante.h@hotmail.es>
2025-07-15 16:28:37 -0400

    cambios reporte traspaso productos17

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -1,31 +1,34 @@
-# report_stock_transfer_charge.py
 # -*- coding: utf-8 -*-
 from odoo import api, models
 
 class StockTransferChargeReport(models.AbstractModel):
     _name = 'report.mi_modulo.stock_transfer_charge'
+    _description = 'Reporte de cargos entre locales'
 
     @api.model
     def _get_report_values(self, docids, data=None):
+        # 1) recupera los pickings seleccionados
         pickings = self.env['stock.picking'].browse(docids or [])
 
-        # 1) Cargo la Tarifa “Interno” o “Interno (CLP)”
-        Tarifa = self.env['product.pricelist']
-        regla  = self.env['product.pricelist.item']
-        precios_interno = {}
+        # 2) busca la pricelist “Interno (CLP)” (o cualquier tarifa que contenga "Interno")
+        Pricelist = self.env['product.pricelist']
+        Item      = self.env['product.pricelist.item']
+        tarifa    = Pricelist.search([('name', 'ilike', 'Interno')], limit=1)
 
-        tarifa = Tarifa.search([('name','ilike','Interno')], limit=1)
+        # 3) construye un dict { move_line_id: precio_interno }
+        precios_interno = {}
         if tarifa:
-            # 2) Cojo todas las reglas de precio fijas
-            items = regla.search([
-                ('pricelist_id','=', tarifa.id),
-                ('applied_on',   '=', '1_product'),
+            reglas = Item.search([
+                ('pricelist_id', '=', tarifa.id),
+                ('applied_on',    '=', '1_product'),
             ])
-            for item in items:
-                fp = item.fixed_price or 0.0
-                # Aplica a todas sus variantes:
-                for v in item.product_tmpl_id.product_variant_ids:
-                    precios_interno[v.id] = fp
+            for regla in reglas:
+                price = regla.fixed_price or 0.0
+                # aplica a cada variante del template
+                for variant in regla.product_tmpl_id.product_variant_ids:
+                    # vamos a indexar por los move_line.id, no por variant,
+                    # así que lo dejamos para lookup en el QWeb mediante ml.id
+                    precios_interno[variant.id] = price
 
         return {
             'doc_model':       'stock.picking',
