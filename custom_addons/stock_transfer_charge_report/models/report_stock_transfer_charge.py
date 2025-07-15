9723ef45fbaaed7d9cac34c97fcc8858145047d3
unknown <dante.h@hotmail.es>
2025-07-15 17:29:22 -0400

    cambios reporte traspaso productos21

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -1,3 +1,4 @@
+# report_stock_transfer_charge.py
 # -*- coding: utf-8 -*-
 from odoo import api, models
 
@@ -10,25 +11,22 @@ class StockTransferChargeReport(models.AbstractModel):
         # 1) recupera los pickings seleccionados
         pickings = self.env['stock.picking'].browse(docids or [])
 
-        # 2) busca la pricelist “Interno (CLP)” (o cualquier tarifa que contenga "Interno")
-        Pricelist = self.env['product.pricelist']
-        Item      = self.env['product.pricelist.item']
-        tarifa    = Pricelist.search([('name', 'ilike', 'Interno')], limit=1)
+        # 2) busca la pricelist “Interno (CLP)” (o cualquier que contenga "Interno")
+        pricelist = self.env['product.pricelist'].search(
+            [('name', 'ilike', 'Interno')], limit=1
+        )
 
         # 3) construye un dict { move_line_id: precio_interno }
         precios_interno = {}
-        if tarifa:
-            reglas = Item.search([
-                ('pricelist_id', '=', tarifa.id),
-                ('applied_on',    '=', '1_product'),
-            ])
-            for regla in reglas:
-                price = regla.fixed_price or 0.0
-                # aplica a cada variante del template
-                for variant in regla.product_tmpl_id.product_variant_ids:
-                    # vamos a indexar por los move_line.id, no por variant,
-                    # así que lo dejamos para lookup en el QWeb mediante ml.id
-                    precios_interno[variant.id] = price
+        if pricelist:
+            for picking in pickings:
+                for ml in picking.move_line_ids_without_package:
+                    qty        = ml.quantity or 0.0
+                    uom_id     = ml.product_uom_id.id
+                    partner_id = picking.partner_id.id or False
+                    precios_interno[ml.id] = pricelist.get_product_price(
+                        ml.product_id, qty, uom_id, partner_id
+                    ) or 0.0
 
         return {
             'doc_model':       'stock.picking',
