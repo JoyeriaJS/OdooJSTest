dd05d092ee6515f5fefe8955dea6cbe3b9c0181a
unknown <dante.h@hotmail.es>
2025-07-15 17:50:08 -0400

    cambios reporte traspaso productos26

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -1,4 +1,3 @@
-# report_stock_transfer_charge.py
 # -*- coding: utf-8 -*-
 from odoo import api, models
 
@@ -8,31 +7,23 @@ class StockTransferChargeReport(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
+        # 1) cargamos los pickings seleccionados
         pickings = self.env['stock.picking'].browse(docids or [])
 
-        # 1) obtenemos la pricelist “Interno (CLP)”
-        Tarifas = self.env['product.pricelist']
-        Regla   = self.env['product.pricelist.item']
-        tarifa = Tarifas.search([('name','ilike','Interno')], limit=1)
+        # 2) buscamos la pricelist “Interno (CLP)” (o la que contenga "Interno")
+        pricelist = self.env['product.pricelist'].search(
+            [('name', 'ilike', 'Interno')], limit=1
+        )
 
-        # 2) buscamos en las reglas el fixed_price
+        # 3) construimos dict { variant_id: precio_interno }
         precios_interno = {}
-        if tarifa:
-            # precargamos todas las reglas aplicables a productos
-            reglas = Regla.search([
-                ('pricelist_id','=', tarifa.id),
-                ('applied_on',   '=', '1_product'),
-            ])
-            # hacemos un dict { template_id: precio }
-            precios_por_template = {
-                r.product_tmpl_id.id: (r.fixed_price or 0.0)
-                for r in reglas
-            }
-            # ahora recorremos cada linea de picking
-            for p in pickings:
-                for ml in p.move_line_ids_without_package:
-                    tmpl_id = ml.product_id.product_tmpl_id.id
-                    precios_interno[ml.id] = precios_por_template.get(tmpl_id, 0.0)
+        if pricelist:
+            # pricelist.item_ids son todas las reglas asociadas
+            for item in pricelist.item_ids.filtered(lambda i: i.applied_on == '1_product'):
+                price = item.fixed_price or 0.0
+                # aplico esa tarifa a cada variante del template
+                for var in item.product_tmpl_id.product_variant_ids:
+                    precios_interno[var.id] = price
 
         return {
             'doc_model':       'stock.picking',
