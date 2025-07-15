1881fb2f621deed510e8484bce99cbbbee07cc7b
unknown <dante.h@hotmail.es>
2025-07-15 12:14:13 -0400

    cambios reporte traspaso productos10

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -1,4 +1,4 @@
-# report_stock_transfer_charge.py
+# models/report_stock_transfer_charge.py
 # -*- coding: utf-8 -*-
 from odoo import api, models
 
@@ -10,26 +10,25 @@ class StockTransferChargeReport(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         pickings = self.env['stock.picking'].browse(docids or [])
 
-        # 1) Recuperamos la pricelist "Interno (CLP)" (o que contenga 'Interno')
+        # 1) Pricelist “Interno (CLP)” o “Interno”
         pricelist = self.env['product.pricelist'].search(
-            [('name', 'ilike', 'Interno')], limit=1
+            [('name', 'in', ['Interno (CLP)', 'Interno'])],
+            limit=1
         )
 
-        # 2) Calculamos el precio interno para cada move_line
         precios_interno = {}
         for picking in pickings:
-            partner_id = picking.partner_id.id or False
+            partner = picking.partner_id.id or False
             for ml in picking.move_line_ids_without_package:
-                qty    = ml.quantity or 0.0
-                uom_id = ml.product_uom_id.id
+                # Odoo17: get_product_price(product, qty, uom, partner)
                 if pricelist:
-                    # get_product_price en Odoo 17 devuelve el precio fijo
-                    price = pricelist.get_product_price(
-                        ml.product_id, qty, uom_id, partner_id
+                    # PASAMOS qty=1.0 para que devuelva precio unitario
+                    precio_unit = pricelist.get_product_price(
+                        ml.product_id, 1.0, ml.product_uom_id.id, partner
                     )
                 else:
-                    price = 0.0
-                precios_interno[ml.id] = price
+                    precio_unit = 0.0
+                precios_interno[ml.id] = precio_unit
 
         return {
             'doc_ids':         pickings.ids,
