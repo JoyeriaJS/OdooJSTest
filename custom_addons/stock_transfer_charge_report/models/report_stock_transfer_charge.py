c34ea77990b548ac652a6a8be30bff2bb0eb4bbf
unknown <dante.h@hotmail.es>
2025-07-15 17:58:13 -0400

    cambios reporte traspaso productos30

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -7,23 +7,30 @@ class StockTransferChargeReport(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # 1) cargamos los pickings seleccionados
+        # 1) Cargo los pickings
         pickings = self.env['stock.picking'].browse(docids or [])
 
-        # 2) buscamos la pricelist “Interno (CLP)” (o la que contenga "Interno")
-        pricelist = self.env['product.pricelist'].search(
-            [('name', 'ilike', 'Interno')], limit=1
-        )
+        # 2) Busco la pricelist “Interno (CLP)” (o cualquier que contenga “Interno”)
+        Tarifas = self.env['product.pricelist']
+        Regla   = self.env['product.pricelist.item']
+        tarifa  = Tarifas.search([('name','ilike','Interno')], limit=1)
 
-        # 3) construimos dict { variant_id: precio_interno }
+        # 3) Recojo todas las reglas de precio internas aplicables a variantes
         precios_interno = {}
-        if pricelist:
-            # pricelist.item_ids son todas las reglas asociadas
-            for item in pricelist.item_ids.filtered(lambda i: i.applied_on == '1_product'):
+        if tarifa:
+            items = Regla.search([
+                ('pricelist_id', '=', tarifa.id),
+                ('applied_on', 'in', ['0_product_variant','1_product']),
+            ])
+            for item in items:
                 price = item.fixed_price or 0.0
-                # aplico esa tarifa a cada variante del template
-                for var in item.product_tmpl_id.product_variant_ids:
-                    precios_interno[var.id] = price
+                # si la regla es por variante concreta:
+                if item.applied_on == '0_product_variant' and item.product_id:
+                    precios_interno[item.product_id.id] = price
+                # si la regla es por plantilla, la aplico a todas sus variantes:
+                elif item.applied_on == '1_product' and item.product_tmpl_id:
+                    for var in item.product_tmpl_id.product_variant_ids:
+                        precios_interno[var.id] = price
 
         return {
             'doc_model':       'stock.picking',
