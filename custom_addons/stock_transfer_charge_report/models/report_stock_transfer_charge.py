b101b12fb952847aaa094bb5b6a785c4d3f70617
unknown <dante.h@hotmail.es>
2025-07-17 10:01:31 -0400

    cambios reporte traspasos tarifa8

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -3,23 +3,28 @@ from odoo import api, models
 
 class StockTransferChargeReport(models.AbstractModel):
     _name = 'report.mi_modulo.stock_transfer_charge'
-    _description = 'Reporte de cargos entre locales (costes)'
+    _description = 'Reporte Simple de Traspasos'
 
     @api.model
-    def _get_report_values(self, docids=None, data=None):
-        # 1) Cargamos los pickings seleccionados
-        pickings = self.env['stock.picking'].browse(docids or [])
+    def _get_report_values(self, docids, data=None):
+        pickings = self.env['stock.picking'].browse(docids) if docids else self.env['stock.picking'].search([])
+        movimientos = []
 
-        # 2) Calculamos un dict { move_line_id: coste_unitario }
-        costos = {}
         for picking in pickings:
             for ml in picking.move_line_ids_without_package:
-                # standard_price es el coste real de inventario
-                costos[ml.id] = ml.product_id.standard_price or 0.0
+                movimientos.append({
+                    'code': ml.product_id.default_code or '',
+                    'name': ml.product_id.display_name or '',
+                    'qty': ml.quantity or 0.0,
+                    'uom': ml.product_uom_id.name or '',
+                    'origen': picking.location_id.display_name or '',
+                    'destino': picking.location_dest_id.display_name or '',
+                    'picking_name': picking.name or '',
+                    'fecha': picking.date_done.strftime('%d/%m/%Y %H:%M:%S') if picking.date_done else '',
+                    'estado': picking.state or '',
+                })
 
         return {
-            'doc_model': 'stock.picking',
-            'doc_ids':   pickings.ids,
-            'docs':      pickings,
-            'costos':    costos,
+            'movimientos': movimientos,
+            'pickings': pickings,
         }
