fb9b3a257c72036970ac4a6e0a2336bf7d548d44
unknown <dante.h@hotmail.es>
2025-07-17 09:48:48 -0400

    cambios reporte traspasos tarifa3

--- custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
+++ custom_addons/stock_transfer_charge_report/models/report_stock_transfer_charge.py
@@ -3,43 +3,23 @@ from odoo import api, models
 
 class StockTransferChargeReport(models.AbstractModel):
     _name = 'report.mi_modulo.stock_transfer_charge'
-    _description = 'Reporte de cargos entre locales'
+    _description = 'Reporte de cargos entre locales (costes)'
 
     @api.model
     def _get_report_values(self, docids=None, data=None):
-        # 1) Cargo los pickings seleccionados
+        # 1) Cargamos los pickings seleccionados
         pickings = self.env['stock.picking'].browse(docids or [])
 
-        # 2) Localizo la moneda CLP
-        clp = self.env['res.currency'].search([('name', '=', 'CLP')], limit=1)
-
-        # 3) Busco la pricelist “Interno (CLP)” (nombre que contenga “Interno” + moneda CLP)
-        Pricelist = self.env['product.pricelist']
-        tarif = Pricelist.search([
-            ('name', 'ilike', 'Interno'),
-            ('currency_id', '=', clp.id),
-        ], limit=1)
-
-        # 4) Construyo { variant_id: precio_interno_fijo }
-        precios_interno = {}
-        if tarif:
-            items = self.env['product.pricelist.item'].search([
-                ('pricelist_id', '=', tarif.id),
-                ('applied_on',    '=', '1_product'),
-            ])
-            for item in items:
-                price = item.fixed_price or 0.0
-                # si apuntan a variante concreta
-                if item.product_id:
-                    precios_interno[item.product_id.id] = price
-                # si apuntan a plantilla, todas sus variantes
-                elif item.product_tmpl_id:
-                    for var in item.product_tmpl_id.product_variant_ids:
-                        precios_interno[var.id] = price
+        # 2) Calculamos un dict { move_line_id: coste_unitario }
+        costos = {}
+        for picking in pickings:
+            for ml in picking.move_line_ids_without_package:
+                # standard_price es el coste real de inventario
+                costos[ml.id] = ml.product_id.standard_price or 0.0
 
         return {
-            'doc_model':       'stock.picking',
-            'doc_ids':         pickings.ids,
-            'docs':            pickings,
-            'precios_interno': precios_interno,
+            'doc_model': 'stock.picking',
+            'doc_ids':   pickings.ids,
+            'docs':      pickings,
+            'costos':    costos,
         }
