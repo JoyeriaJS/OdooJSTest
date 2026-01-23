07543522f88340e4b7d4977d47f7aa19ee3e467c
unknown <dante.h@hotmail.es>
2026-01-23 09:44:19 -0300

    cambiosprueba2modulonuev13

--- custom_addons/pos_discount_authorized/__manifest__.py
+++ custom_addons/pos_discount_authorized/__manifest__.py
@@ -4,6 +4,7 @@
     "version": "1.0",
     "depends": ["point_of_sale"],
     "data": [
+        'security/pos_discount_model.xml',
         "security/ir.model.access.csv",
         'models/pos_discount_code_model.xml',
         "views/discount_code_views.xml",
