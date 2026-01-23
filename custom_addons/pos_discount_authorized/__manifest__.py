c5bbc30b19f01df4ce329aa99d2f939c8d47d777
unknown <dante.h@hotmail.es>
2026-01-23 12:45:46 -0300

    cambiosprueba2modulonuev52

--- custom_addons/pos_discount_authorized/__manifest__.py
+++ custom_addons/pos_discount_authorized/__manifest__.py
@@ -12,7 +12,7 @@
     "assets": {
         "point_of_sale._assets": [
             "/pos_discount_authorized/static/src/js/discount_code.js",
-            "/pos_discount_authorized/static/src/xml/discount_button_template.xml",
+            "/pos_discount_authorized/static/src/xml/discount_button.xml",
         ]
     },
     "installable": True
