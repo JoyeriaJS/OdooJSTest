fd10a92ce85dccd71a3d2250dd399ce4a7878eb1
unknown <dante.h@hotmail.es>
2026-01-26 11:20:32 -0300

    cambiosmodulocambio10

--- custom_addons/pos_discount_authorized/__manifest__.py
+++ custom_addons/pos_discount_authorized/__manifest__.py
@@ -1,22 +1,20 @@
+
 {
     "name": "POS Discount Authorized",
     "version": "1.0",
+    "license": "LGPL-3",
     "depends": ["point_of_sale"],
     "data": [
         "security/pos_discount_model.xml",
         "security/ir.model.access.csv",
         "models/pos_discount_code_model.xml",
-        "views/discount_code_views.xml",
+        "views/discount_code_views.xml"
     ],
     "assets": {
-    "point_of_sale.assets_prod": [
-        "pos_discount_authorized/static/src/js/discount_code.js",
-        "pos_discount_authorized/static/src/xml/discount_button.xml"
-    ],
-    "point_of_sale.assets_debug": [
-        "pos_discount_authorized/static/src/js/discount_code.js",
-        "pos_discount_authorized/static/src/xml/discount_button.xml"
-    ]
+        "point_of_sale._assets_pos": [
+            "pos_discount_authorized/static/src/js/discount_code.js",
+            "pos_discount_authorized/static/src/xml/discount_button.xml"
+        ]
     },
     "installable": True,
 }
