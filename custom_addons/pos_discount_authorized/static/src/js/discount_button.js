b6a8df9907f82c8912186f8afc690b50822c2aac
unknown <dante.h@hotmail.es>
2026-01-29 15:30:57 -0300

    cambiospruebaposmod58

--- custom_addons/pos_discount_authorized/static/src/js/discount_button.js
+++ custom_addons/pos_discount_authorized/static/src/js/discount_button.js
@@ -1,27 +1,22 @@
 /** @odoo-module **/
 
-import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
+import { PosComponent } from "@point_of_sale/app/components/base/pos_component";
 import { registry } from "@web/core/registry";
-import { PosComponent } from "@point_of_sale/app/components/pos_component/pos_component";
-import { usePos } from "@point_of_sale/app/store/pos_hook";
 
 export class PosDiscountButton extends PosComponent {
     setup() {
-        this.pos = usePos();
-        console.log("🔥 Botón de Descuento CARGADO en ControlButtons");
+        super.setup();
+        console.log("🔥 Botón de Descuento CARGADO correctamente en Odoo 17 Community");
     }
 
     onClick() {
-        this.showPopup("ConfirmPopup", {
-            title: "Descuento Autorizado",
-            body: "El botón fue presionado correctamente."
-        });
+        alert("Botón funcionando!");
     }
 }
 
 PosDiscountButton.template = "PosDiscountButton";
 
-registry.category("pos_control_buttons").add("PosDiscountButton", {
+registry.category("pos_screens").add("PosDiscountButton", {
     component: PosDiscountButton,
-    position: 6,  // lo inserta después de los otros botones
+    position: ["product-buttons"],  // ← aparecerá en la zona correcta
 });
