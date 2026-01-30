f34e62513f5ef6a792355b20b31480b00bc7e94e
unknown <dante.h@hotmail.es>
2026-01-30 11:06:54 -0300

    cambiosencuentraelboton15

--- custom_addons/pos_discount_authorized/__manifest__.py
+++ custom_addons/pos_discount_authorized/__manifest__.py
@@ -8,12 +8,14 @@
         "security/ir.model.access.csv",
         "views/pos_discount_code_views.xml",
     ],
+
     "assets": {
-        "point_of_sale.assets": [
+        "point_of_sale.assets_prod": [
             "pos_discount_authorized/static/src/js/discount_button.js",
             "pos_discount_authorized/static/src/xml/product_screen.xml",
         ],
     },
+
     "installable": True,
     "license": "LGPL-3",
 }
