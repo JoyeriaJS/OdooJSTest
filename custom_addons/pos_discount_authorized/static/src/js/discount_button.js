f26e7fe088f617ecf4174a0e1997c92632b3c349
unknown <dante.h@hotmail.es>
2026-02-02 09:20:11 -0300

    cambiosvscmanifet6

--- custom_addons/pos_discount_authorized/static/src/js/discount_button.js
+++ custom_addons/pos_discount_authorized/static/src/js/discount_button.js
@@ -1,14 +1,31 @@
-/** @odoo-module **/
+/** @odoo-module */
 
 import { patch } from "@web/core/utils/patch";
 import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product_screen";
-patch(ActionPad.prototype, {
+import { useService } from "@web/core/utils/hooks";
+
+patch(ProductScreen.prototype, {
     setup() {
         super.setup();
-        console.log("🔥 Botón de descuento cargado en ActionPad (v17 Community) 🔥");
+        this.popup = useService("popup");
+        console.log("🔥 BOTÓN DE DESCUENTO ACTIVADO EN ODOO 17 COMMUNITY");
     },
 
-    onClickDiscount() {
-        alert("Descuento funcionando en Community!");
+    async onClickDiscount() {
+        const result = await this.popup.add({
+            type: "text",
+            title: "Código de autorización",
+            body: "Ingresa el código para aplicar descuento",
+        });
+
+        if (result && result.confirmed) {
+            const code = result.payload;
+
+            if (code === "1234") {
+                alert("Código correcto. Puedes aplicar el descuento.");
+            } else {
+                alert("Código incorrecto.");
+            }
+        }
     },
 });
