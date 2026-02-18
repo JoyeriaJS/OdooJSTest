e5897dbeacac00b9ec128e1fec86aafc9951d2c3
unknown <dante.h@hotmail.es>
2026-02-18 13:01:19 -0300

    cambiosmodulocuaderno40

--- custom_addons/pos_vendedoras_control/models/venta_vendedora_line.py
+++ custom_addons/pos_vendedoras_control/models/venta_vendedora_line.py
@@ -6,45 +6,51 @@ class VentaVendedoraLine(models.Model):
     _name = 'venta.vendedora.line'
     _description = 'Detalle de Tickets del Cierre'
 
-    _sql_constraints = [
-        ('unique_ticket', 'unique(pos_order_id)',
-         'Este ticket ya fue registrado en otro cierre.')
-    ]
-
     cierre_id = fields.Many2one(
         'venta.vendedora',
-        string="Cierre",
-        required=True,
         ondelete='cascade'
     )
+
+    # 👇 AHORA ES CHAR
+    pos_reference_input = fields.Char(string="Número Ticket", required=True)
 
     pos_order_id = fields.Many2one(
         'pos.order',
         string="Orden POS",
-        required=True,
-        domain="[('state','=','paid')]",
+        readonly=True
     )
 
-    pos_reference = fields.Char(
-        related='pos_order_id.pos_reference',
-        string="Número Ticket",
-        store=True
-    )
+    efectivo = fields.Float(string="Efectivo", readonly=True)
+    transferencia = fields.Float(string="Transferencia", readonly=True)
+    tarjeta = fields.Float(string="Tarjeta", readonly=True)
 
-    efectivo = fields.Float(string="Efectivo")
-    transferencia = fields.Float(string="Transferencia")
-    tarjeta = fields.Float(string="Tarjeta")
+    _sql_constraints = [
+        ('unique_ticket',
+         'unique(pos_reference_input)',
+         'Este ticket ya fue agregado.')
+    ]
 
-    @api.onchange('pos_order_id')
-    def _onchange_pos_order(self):
-        if not self.pos_order_id:
+    # 🔥 BUSCAR ORDEN AUTOMÁTICAMENTE
+    @api.onchange('pos_reference_input')
+    def _onchange_buscar_ticket(self):
+
+        if not self.pos_reference_input:
             return
+
+        order = self.env['pos.order'].search([
+            ('pos_reference', '=', self.pos_reference_input)
+        ], limit=1)
+
+        if not order:
+            raise ValidationError("No existe una orden POS con ese número.")
+
+        self.pos_order_id = order.id
 
         efectivo = 0.0
         transferencia = 0.0
         tarjeta = 0.0
 
-        for payment in self.pos_order_id.payment_ids:
+        for payment in order.payment_ids:
             metodo = payment.payment_method_id.name.strip()
 
             if metodo == 'Venta Efectivo':
