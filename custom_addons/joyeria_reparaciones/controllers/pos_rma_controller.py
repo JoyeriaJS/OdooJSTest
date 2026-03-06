599e9b4d8b7e24a2ddd849f179c55b814795fade
unknown <dante.h@hotmail.es>
2026-03-06 12:18:05 -0300

    cambioscontrollerrmaproduct190

--- custom_addons/joyeria_reparaciones/controllers/pos_rma_controller.py
+++ custom_addons/joyeria_reparaciones/controllers/pos_rma_controller.py
@@ -4,21 +4,31 @@ from odoo.http import request
 
 class PosRMAController(http.Controller):
 
-    @http.route('/pos/buscar_rma', type='json', auth='user', methods=['POST'])
-    def buscar_rma(self):
+    @http.route('/pos/buscar_rma', type='json', auth='user', methods=['POST'], csrf=False)
+    def buscar_rma(self, **kwargs):
 
-        data = request.get_json_data()
+        # Obtener rma desde JSON
+        rma_input = kwargs.get('rma')
 
-        rma = data.get("rma")
-
-        if not rma:
+        if not rma_input:
             return {
                 "success": False,
-                "message": "Debe ingresar un RMA"
+                "message": "Debe ingresar un número de RMA"
             }
 
+        rma_input = str(rma_input).strip().upper()
+
+        # Si escriben solo número (ej 1162)
+        if not rma_input.startswith("RMA/"):
+            try:
+                numero = int(rma_input)
+                rma_input = f"RMA/{numero:05d}"
+            except:
+                pass
+
+        # Buscar reparación
         reparacion = request.env['joyeria.reparacion'].sudo().search([
-            ('name', '=', rma)
+            ('name', '=', rma_input)
         ], limit=1)
 
         if not reparacion:
@@ -27,13 +37,14 @@ class PosRMAController(http.Controller):
                 "message": "RMA no encontrado"
             }
 
-        if not reparacion.abono:
+        if not reparacion.abono or reparacion.abono <= 0:
             return {
                 "success": False,
-                "message": "El RMA no tiene valor de abono"
+                "message": "RMA sin abono"
             }
 
         return {
             "success": True,
-            "precio": reparacion.abono
+            "precio": reparacion.abono,
+            "rma": reparacion.name
         }
