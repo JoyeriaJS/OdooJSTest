a60600d6fccbbb6a0401a0ef2e117d34897876b1
unknown <dante.h@hotmail.es>
2026-03-06 10:46:07 -0300

    cambioscontrollerrmaproduct112

--- custom_addons/joyeria_reparaciones/controllers/pos_rma_controller.py
+++ custom_addons/joyeria_reparaciones/controllers/pos_rma_controller.py
@@ -5,24 +5,18 @@ from odoo.http import request
 class PosRMAController(http.Controller):
 
     @http.route('/pos/buscar_rma', type='json', auth='user')
-    def buscar_rma(self, **kwargs):
-
-        rma = kwargs.get('rma')
-
-        if not rma:
-            return False
+    def buscar_rma(self, rma):
 
         reparacion = request.env['joyeria.reparacion'].sudo().search([
             ('name', '=', rma)
         ], limit=1)
 
         if not reparacion:
-            return False
-
-        if not reparacion.abono:
-            return False
+            return {
+                "success": False
+            }
 
         return {
-            'rma': reparacion.name,
-            'abono': reparacion.abono
+            "success": True,
+            "abono": reparacion.abono
         }
