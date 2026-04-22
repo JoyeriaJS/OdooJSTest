27f82789da941a01b4a9ab8fbc10f9b4c9682809
unknown <dante.h@hotmail.es>
2026-04-21 21:25:33 -0400

    cambios_arreglosmanifest75

--- custom_addons/joyeria_reparaciones/__manifest__.py
+++ custom_addons/joyeria_reparaciones/__manifest__.py
@@ -11,7 +11,7 @@
         'sale',
         'contacts',
         'point_of_sale',
-        'report_xlsx',
+        #'report_xlsx',
         'mail',
     ],
 
@@ -34,26 +34,26 @@
 
         # 🔹 REPORTES (al final siempre)
         #'report/report.xml',
-        'report/report_qrcode_template.xml',
-        'report/reporte_responsable_report.xml',
-        'report/reporte_reparaciones_responsable.xml',
-        'report/report_actions.xml',
-        'report/report_etiqueta_usuario.xml',
-        'report/report_etiqueta_vendedora.xml',
+        #'report/report_qrcode_template.xml',
+        #'report/reporte_responsable_report.xml',
+        #'report/reporte_reparaciones_responsable.xml',
+        #'report/report_actions.xml',
+        #'report/report_etiqueta_usuario.xml',
+        #'report/report_etiqueta_vendedora.xml',
 
-        'report/report_salida_taller_xlsx.xml',
-        'report/report_sales_by_store_xlsx.xml',
-        'report/report_sales_by_vendedora_xlsx.xml',
+        #'report/report_salida_taller_xlsx.xml',
+        #'report/report_sales_by_store_xlsx.xml',
+        #'report/report_sales_by_vendedora_xlsx.xml',
 
-        'report/reporte_salida_taller_report_action.xml',
-        'report/reporte_salida_taller_templates.xml',
-        'report/report_sales_by_store.xml',
-        'report/report_sales_by_store_template.xml',
-        'report/report_sales_by_vendedora.xml',
-        'report/report_sales_by_vendedora_template.xml',
+        #'report/reporte_salida_taller_report_action.xml',
+        #'report/reporte_salida_taller_templates.xml',
+        #'report/report_sales_by_store.xml',
+        #'report/report_sales_by_store_template.xml',
+        #'report/report_sales_by_vendedora.xml',
+        #'report/report_sales_by_vendedora_template.xml',
 
-        'report/report_monthly_rma_pos_action.xml',
-        'report/report_monthly_rma_pos_template.xml',
+        #'report/report_monthly_rma_pos_action.xml',
+        #'report/report_monthly_rma_pos_template.xml',
     ],
 
     # 🔥 JS BIEN CARGADO
