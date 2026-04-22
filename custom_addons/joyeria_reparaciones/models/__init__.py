23644e66101af829e15fc7d75985fdbcd1ebe2e3
unknown <dante.h@hotmail.es>
2026-04-21 21:03:34 -0400

    cambios_arreglosmanifest30

--- custom_addons/joyeria_reparaciones/models/__init__.py
+++ custom_addons/joyeria_reparaciones/models/__init__.py
@@ -3,15 +3,15 @@ from . import joyeria_producto
 
 # 🔥 AGREGA ESTOS (faltaban)
 #from . import reparacion_auth_code
-from . import reparacion_code
-from . import pos_discount
-from . import pos_order
-from . import pos_order_line
-from . import pos_session
+#from . import reparacion_code
+#grom . import pos_discount
+#from . import pos_order
+#from . import pos_order_line
+#from . import pos_session
 
 # wizards
-#from . import wizard_reporte_responsables
-#from . import reporte_salida_taller_wizard
+
+
 from . import report_salida_taller_xlsx
 
 # reports
