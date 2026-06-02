7af977b9789b6e694d0246177382f924344141b6
unknown <dante.h@hotmail.es>
2026-06-02 14:34:00 -0400

    cambioscrearcotizacionesodoo10

--- custom_addons/joyeria_cotizaciones/__manifest__.py
+++ custom_addons/joyeria_cotizaciones/__manifest__.py
@@ -12,7 +12,7 @@
     ],
     "data": [
         "security/ir.model.access.csv",
-        #"views/cotizacion_views.xml",
+        "views/cotizacion_views.xml",
         "data/sequence.xml",
         "report/cotizacion_report.xml",
         "report/cotizacion_template.xml",
