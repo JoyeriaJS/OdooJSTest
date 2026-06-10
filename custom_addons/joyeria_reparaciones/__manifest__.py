7068dae997a12b990418bd370cff1a4761d77923
unknown <dante.h@hotmail.es>
2026-06-10 11:25:07 -0400

    cambiosnombressalidtaller37

--- custom_addons/joyeria_reparaciones/__manifest__.py
+++ custom_addons/joyeria_reparaciones/__manifest__.py
@@ -4,16 +4,15 @@
     'summary': 'Gestión de órdenes de reparación de joyas',
     'author': 'DR',
     'category': 'Operations',
-    'depends': ['base', 'product', 'sale', 'contacts','point_of_sale', 'report_xlsx'
-                'mail'],
+    'depends': ['base', 'product', 'sale', 'contacts','point_of_sale', 'report_xlsx','mail'],
     'data': [
         'security/joyeria_security.xml',
         'security/ir.model.access.csv',
         'views/reparacion_js.xml',
         #'data/ir_sequence_data.xml',
         #'data/joyeria_data.xml',
+        'report/report_qrcode_template.xml',
         #'report/report.xml',  # ← este es el que importa
-        #'report/report_qrcode_template.xml',
         #'report/reporte_responsable_report.xml',
         #'static/src/reparacion_form.js',
         #'report/reporte_reparaciones_responsable.xml',
@@ -32,12 +31,12 @@
         "views/reparacion_confirm_view.xml",
         'views/pos_order_line_views.xml',
         'views/descuentos_pos_views.xml',
-        #'views/pos_menu.xml',
+        'views/pos_menu.xml',
         
         
 
         #'report/report_salida_taller_xlsx.xml',
-        'report/report_sales_by_store_xlsx.xml',
+        #'report/report_sales_by_store_xlsx.xml',
         #'report/report_sales_by_vendedora_xlsx.xml',
 
         #'report/reporte_salida_taller_report_action.xml',
@@ -51,7 +50,7 @@
         #'report/report_monthly_rma_pos_template.xml',  
         
     ],
-    'depends': ['point_of_sale'],
+     'depends': ['point_of_sale'],
     'assets': {
         'point_of_sale._assets_pos': [
             #'joyeria_reparaciones/static/src/xml/pos_rma_line.xml',
